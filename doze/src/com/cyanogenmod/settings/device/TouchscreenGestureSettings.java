fdff2af64d5911ced20bc6fe57228d1958211d91
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-13 18:33:52 +0800

    samsung: doze: Clean up ambient display pref dependencies
     * Use SecureSettingSwitchPreference for Ambient Display
     * Use the XML preference dependency attribute
    
    Change-Id: I3480af71e334110aed834a53f49b33a853f16316

--- doze/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
+++ doze/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -26,32 +26,19 @@ import org.cyanogenmod.internal.util.ScreenType;
 
 public class TouchscreenGestureSettings extends PreferenceFragment {
 
-    private static final String KEY_AMBIENT_DISPLAY_ENABLE = "ambient_display_enable";
     private static final String KEY_HAND_WAVE = "gesture_hand_wave";
-    private static final String KEY_GESTURE_POCKET = "gesture_pocket";
     private static final String KEY_PROXIMITY_WAKE = "proximity_wake_enable";
 
-    private SwitchPreference mAmbientDisplayPreference;
     private SwitchPreference mHandwavePreference;
-    private SwitchPreference mPocketPreference;
     private SwitchPreference mProximityWakePreference;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.gesture_panel);
-        boolean dozeEnabled = isDozeEnabled();
-        mAmbientDisplayPreference =
-            (SwitchPreference) findPreference(KEY_AMBIENT_DISPLAY_ENABLE);
-        // Read from DOZE_ENABLED secure setting
-        mAmbientDisplayPreference.setChecked(dozeEnabled);
-        mAmbientDisplayPreference.setOnPreferenceChangeListener(mAmbientDisplayPrefListener);
+
         mHandwavePreference =
             (SwitchPreference) findPreference(KEY_HAND_WAVE);
-        mHandwavePreference.setEnabled(dozeEnabled);
         mHandwavePreference.setOnPreferenceChangeListener(mProximityListener);
-        mPocketPreference =
-            (SwitchPreference) findPreference(KEY_GESTURE_POCKET);
-        mPocketPreference.setEnabled(dozeEnabled);
         mProximityWakePreference =
             (SwitchPreference) findPreference(KEY_PROXIMITY_WAKE);
         mProximityWakePreference.setOnPreferenceChangeListener(mProximityListener);
@@ -66,30 +53,6 @@ public class TouchscreenGestureSettings extends PreferenceFragment {
             getListView().setPadding(0, 0, 0, 0);
         }
     }
-
-    private boolean enableDoze(boolean enable) {
-        return Settings.Secure.putInt(getContext().getContentResolver(),
-                Settings.Secure.DOZE_ENABLED, enable ? 1 : 0);
-    }
-
-    private boolean isDozeEnabled() {
-        return Settings.Secure.getInt(getContext().getContentResolver(),
-                Settings.Secure.DOZE_ENABLED, 1) != 0;
-    }
-
-    private Preference.OnPreferenceChangeListener mAmbientDisplayPrefListener =
-        new Preference.OnPreferenceChangeListener() {
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-            boolean enable = (boolean) newValue;
-            boolean ret = enableDoze(enable);
-            if (ret) {
-                mHandwavePreference.setEnabled(enable);
-                mPocketPreference.setEnabled(enable);
-            }
-            return ret;
-        }
-    };
 
     private Preference.OnPreferenceChangeListener mProximityListener =
         new Preference.OnPreferenceChangeListener() {
