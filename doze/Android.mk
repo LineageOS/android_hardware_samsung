f62a187de762478f116f6a314b0561bf61087cdd
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-11-23 18:13:45 -0800

    samsung: doze: Commonize strings translations
     * Translations moved to the new project
        android_packages_resources_devicesettings
    
    Change-Id: I06536915e1963a2fe464c3d6c46dd8728aba0cb4
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- doze/Android.mk
+++ doze/Android.mk
@@ -20,6 +20,7 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
+    $(LOCAL_PATH)/../../../../packages/resources/devicesettings/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/preference/res \
