04c6429ca4c657783ead1e7ad3f9432c94b9d776
Christopher N. Hesse <raymanfx@gmail.com>
2017-01-04 01:27:48 +0100

    dtbhtool: Rename unpackbootimg to avoid conflicts
    Change-Id: Iee1146dc6eca01dc50ac3e41e0b12a41a33064c1

--- dtbhtool/Android.mk
+++ dtbhtool/Android.mk
@@ -20,8 +20,8 @@ LOCAL_MODULE := mkdtbhbootimg
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := unpackbootimg.c
-LOCAL_MODULE := unpackbootimg
+LOCAL_SRC_FILES := unpackdtbhbootimg.c
+LOCAL_MODULE := unpackdtbhbootimg
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -46,11 +46,11 @@ LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := unpackbootimg.c
+LOCAL_SRC_FILES := unpackdtbhbootimg.c
 LOCAL_STATIC_LIBRARIES := libcutils libc
-LOCAL_MODULE := utility_unpackbootimg
+LOCAL_MODULE := utility_unpackdtbhbootimg
 LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE_STEM := unpackbootimg
+LOCAL_MODULE_STEM := unpackdtbhbootimg
 LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
 LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
