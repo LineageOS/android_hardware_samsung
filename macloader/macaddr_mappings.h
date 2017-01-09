8b4ba0d6b6143065dcec09c25b334a34e2b2045d
Davis Mosenkovs <davikovs@gmail.com>
2017-01-09 12:11:31 -0800

    Added new Murata MAC range
    OUI a0:c9:a0 was observed on hero devices bought in December 2017
    
    Change-Id: I0a2404780b1b5b017bb89468d235a2174a245989
    (cherry picked from commit 939c81d0e0581a08b896636ce0f1d5bb96946dc9)

--- macloader/macaddr_mappings.h
+++ macloader/macaddr_mappings.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017, The LineageOS Project
+ * Copyright (C) 2017-2018, The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,6 +68,7 @@ static const struct company_range murata_ranges = {
         "8c:45:00",
         "90:b6:86",
         "98:f1:70",
+        "a0:c9:a0",
         "a0:cc:2b",
         "a4:08:ea",
         "b0:72:bf",
