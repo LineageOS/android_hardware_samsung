e4aa30bbfa0f3e1a03874a885bf3467d16f4df70
Christopher N. Hesse <raymanfx@gmail.com>
2017-02-02 20:48:56 +0100

    WIP audio: Initialize the radio voice session
    This just allocates the structre and creates the RIL connection. This is
    per audio_device structure to make sure we to not run into issues with
    audioserver threading.
    
    Change-Id: I42e1b7ae57579f39c5f76566ef5b67d4e2c13e3f

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -48,6 +48,7 @@
 #include <audio_effects/effect_ns.h>
 #include "audio_hw.h"
 #include "compress_offload.h"
+#include "voice.h"
 
 #include "sound/compress_params.h"
 
@@ -4059,9 +4060,12 @@ static int adev_close(hw_device_t *device)
 
     audio_route_free(adev->mixer.audio_route);
 
+    voice_session_deinit(adev->voice.session);
+
     audio_device_ref_count--;
     free(adev->snd_dev_ref_cnt);
     free(device);
+
     return 0;
 }
 
@@ -4198,6 +4202,16 @@ static int adev_open(const hw_module_t *module, const char *name,
         }
     }
 
+    adev->voice.session = voice_session_init();
+    if (adev->voice.session == NULL) {
+        ALOGE("%s: Failed to initialize voice session data", __func__);
+
+        free(adev->snd_dev_ref_cnt);
+        free(adev);
+
+        *device = NULL;
+        return -EINVAL;
+    }
 
     *device = &adev->device.common;
 
