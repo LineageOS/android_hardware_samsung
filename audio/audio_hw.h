39521070ba518c03d6a8a2a903fc547e3a33ee6e
Andreas Schneider <asn@cryptomilk.org>
2017-02-02 18:29:12 +0100

    audio: Create a voice structure to group values
    Change-Id: I184d39460aa8a9a3ee5efc5fe3aa63e99a2e9d12

--- audio/audio_hw.h
+++ audio/audio_hw.h
@@ -377,10 +377,14 @@ struct audio_device {
     audio_mode_t            mode;
     struct stream_in*       active_input;
     struct stream_out*      primary_output;
-    bool                    in_call;
-    float                   voice_volume;
     bool                    mic_mute;
-    bool                    bluetooth_nrec;
+
+    struct {
+        bool                in_call;
+        float               volume;
+        bool                bluetooth_nrec;
+    } voice;
+
     int*                    snd_dev_ref_cnt;
     struct listnode         usecase_list;
     bool                    speaker_lr_swap;
