125eb5c32331920c754e0f47067288791f281266
Christopher N. Hesse <raymanfx@gmail.com>
2017-02-02 20:48:56 +0100

    audio: Initialize the radio voice session
    This just allocates the structre and creates the RIL connection. This is
    per audio_device structure to make sure we to not run into issues with
    audioserver threading.
    
    Change-Id: I42e1b7ae57579f39c5f76566ef5b67d4e2c13e3f

--- audio/audio_hw.h
+++ audio/audio_hw.h
@@ -383,6 +383,7 @@ struct audio_device {
         bool                in_call;
         float               volume;
         bool                bluetooth_nrec;
+        void                *session;
     } voice;
 
     int*                    snd_dev_ref_cnt;
