71fb56ff3a1d540374dfb182db13ea875139c088
Jesse Chan <jc@linux.com>
2017-02-05 21:18:09 +0800

    add Samsung BAUTH Fingerprint HAL
    Change-Id: I3c0d3e2a0f9b77f0202cbdb0a75a00b760afcd8c

--- Android.mk
+++ Android.mk
@@ -40,6 +40,7 @@ include $(SAM_ROOT)/AdvancedDisplay/Android.mk
 include $(SAM_ROOT)/audio/Android.mk
 include $(SAM_ROOT)/consumerir/Android.mk
 include $(SAM_ROOT)/dtbhtool/Android.mk
+include $(SAM_ROOT)/fingerprint/Android.mk
 include $(SAM_ROOT)/liblights/Android.mk
 include $(SAM_ROOT)/modemloader/Android.mk
 include $(SAM_ROOT)/power/Android.mk
