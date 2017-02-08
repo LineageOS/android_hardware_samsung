d7dfc88e4522997d1c2e2cb13ac44b8e0af9f5a9
Andreas Schneider <asn@cryptomilk.org>
2017-02-08 16:58:22 +0100

    audio: Do not segfault in out_get_presentation_position()
    When a phone call is hung up, we segfault here. This is probably because
    we open the PCM in the voice.c so it is not available here. I don't know
    how to fix this yet.
    
    Change-Id: I927504b7962b096c0d4c3642b48aee55c85ec013

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -3085,19 +3085,31 @@ static int out_get_presentation_position(const struct audio_stream_out *stream,
             struct pcm_device *pcm_device = node_to_item(list_head(&out->pcm_dev_list),
                                                    struct pcm_device, stream_list_node);
 
-            if (pcm_get_htimestamp(pcm_device->pcm, &avail, timestamp) == 0) {
-                size_t kernel_buffer_size = out->config.period_size * out->config.period_count;
-                int64_t signed_frames = out->written - kernel_buffer_size + avail;
-                /* This adjustment accounts for buffering after app processor.
-                   It is based on estimated DSP latency per use case, rather than exact. */
-                signed_frames -=
-                    (render_latency(out->usecase) * out->sample_rate / 1000000LL);
+            if (pcm_device->pcm != NULL) {
+                ret = pcm_get_htimestamp(pcm_device->pcm, &avail, timestamp);
+                if (ret == 0) {
+                    size_t kernel_buffer_size = out->config.period_size * out->config.period_count;
+                    int64_t signed_frames = out->written - kernel_buffer_size + avail;
+                    /* This adjustment accounts for buffering after app processor.
+                       It is based on estimated DSP latency per use case, rather than exact. */
+                    signed_frames -=
+                        (render_latency(out->usecase) * out->sample_rate / 1000000LL);
 
-                /* It would be unusual for this value to be negative, but check just in case ... */
-                if (signed_frames >= 0) {
-                    *frames = signed_frames;
-                    ret = 0;
+                    /* It would be unusual for this value to be negative, but check just in case ... */
+                    if (signed_frames >= 0) {
+                        *frames = signed_frames;
+                        ret = 0;
+                    }
                 }
+            } else {
+                /*
+                 * This happens after a phone call. We have our voice pcm_config
+                 * in voice.c so it isn't open here. It is also possible that
+                 * we need to walk the list and find the first pcm_device with
+                 * the pcm not being NULL.
+                 */
+                ALOGE("%s: No PCM open for stream, check FIXME in the code",
+                      __func__);
             }
         }
     }
