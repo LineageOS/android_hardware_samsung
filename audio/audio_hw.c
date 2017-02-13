144e70ba04169410c4642afb0d3227a0a5f22350
Andreas Schneider <asn@cryptomilk.org>
2017-02-13 15:19:36 +0100

    audio: Fix device table access in get_snd_device_name()
    We have a value for SND_DEVICE_NONE so we should use that. There is no
    field for SND_DEVICE_MIN so we need to check it is bigger.
    
    Change-Id: I041930c8c4b552d45804a178aeb1aa981ae7c13f

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -440,7 +440,8 @@ static const char *get_snd_device_name(snd_device_t snd_device)
 {
     const char *name = NULL;
 
-    if (snd_device >= SND_DEVICE_MIN && snd_device < SND_DEVICE_MAX)
+    if (snd_device == SND_DEVICE_NONE ||
+        (snd_device > SND_DEVICE_MIN && snd_device < SND_DEVICE_MAX))
         name = device_table[snd_device];
 
     ALOGE_IF(name == NULL, "%s: invalid snd device %d", __func__, snd_device);
