ef8af32ef3e2ecb94fc34cfd01751c451d051a8f
Christopher N. Hesse <raymanfx@gmail.com>
2017-03-12 17:11:04 +0100

    power: Rework governor path abstraction
    Construct the paths for the driver based on the device
    specific header data.
    
    Change-Id: Ic7c72a2bbe6f199e1297f41817840059595f516d

--- power/include/samsung_power.h
+++ power/include/samsung_power.h
@@ -24,14 +24,8 @@
  * symlink to the locations given here, or override this header in your
  * device tree.
  */
-#define BOOST_PATH "/sys/devices/system/cpu/cpu0/cpufreq/interactive/boost"
-#define BOOSTPULSE_PATH "/sys/devices/system/cpu/cpu0/cpufreq/interactive/boostpulse"
 
-#define IO_IS_BUSY_PATH "/sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy"
-
-#define CPU0_HISPEED_FREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq"
-#define CPU0_MAX_FREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
-#define CPU4_HISPEED_FREQ_PATH "/sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq"
-#define CPU4_MAX_FREQ_PATH "/sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq"
+#define CPU0_SYSFS_PATH "/sys/devices/system/cpu/cpu0"
+#define CPU4_SYSFS_PATH "/sys/devices/system/cpu/cpu4"
 
 #endif // SAMSUNG_POWER_H
