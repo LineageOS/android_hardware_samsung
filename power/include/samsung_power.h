ed102823965898e38ade2273490b35303890e136
ShevT <shev_t@mail.ru>
2017-03-19 14:22:48 +0300

    power: Do not use cpu4, if not declared CPU4_SYSFS_PATH
    * There is no need to duplicate paths (in samsung_power.h) and double-write values.
    * We do not need to make additional checks for the existence of a path.
    
    Change-Id: I974a9b1432aaf43344c7d0a37249c19b3f959dbb

--- power/include/samsung_power.h
+++ power/include/samsung_power.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,8 +26,14 @@
  */
 
 #define CPU0_SYSFS_PATH "/sys/devices/system/cpu/cpu0"
-#define CPU4_SYSFS_PATH "/sys/devices/system/cpu/cpu4"
 #define CPU0_INTERACTIVE_PATH "/sys/devices/system/cpu/cpu0/cpufreq/interactive"
+
+/* CPU4 Paths.- 
+ *
+ * Do not define these if your device does not have CPU4 nodes
+*/
+
+#define CPU4_SYSFS_PATH "/sys/devices/system/cpu/cpu4"
 #define CPU4_INTERACTIVE_PATH "/sys/devices/system/cpu/cpu4/cpufreq/interactive"
 
 #endif // SAMSUNG_POWER_H
