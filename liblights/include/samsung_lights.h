5084d7b90808f467b2b7a835531642554bf1d8de
Dominggoes Isakh <drjisakh@gmail.com>
2017-05-26 23:43:49 +0200

    liblights: Add BLN support
    Change-Id: I3745b7c4ca7adadd62e116c71cd7357a4e8ed015

--- liblights/include/samsung_lights.h
+++ liblights/include/samsung_lights.h
@@ -29,6 +29,7 @@
 #define PANEL_MAX_BRIGHTNESS_NODE "/sys/class/backlight/panel/max_brightness"
 #define BUTTON_BRIGHTNESS_NODE "/sys/class/sec/sec_touchkey/brightness"
 #define LED_BLINK_NODE "/sys/class/sec/led/led_blink"
+#define LED_BLN_NODE "/sys/class/misc/backlightnotification/notification_led"
 
 /*
  * Brightness adjustment factors
