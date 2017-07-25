471cac21aae911c498b803c4272ff45ce862b692
Martin Bouchet <tincho5588@gmail.com>
2017-07-25 03:30:58 -0300

    ril: add a common customization subclass for exynos devices
    This is possible due to a couple of facts:
    * The ril class is connected to our OSS libril through a parcel.
    
    * Ril customization should be done on a by modem basis,
    taking into consideration the read/writes libril does
    to the parcel.
    
    * The modems are grouped together into 2 big categories,
    NEXT_GEN_MODEMs, and XMM_6x6x (3G) modems. All of the next gen modems
    have the same read/writes to the parcel. Same applies for non next gen modems.
    
    * Based on this, we can create a common ril subclass that
    is totally aligned with libril, using 2 system properties
    to condition some read/writes to the parcel that are not
    needed on non next gen modems.
    
    * These 2 system properties are being set automatically based on the
    BOARD_MODEM_TYPE value defined in the device tree.
    
    Change-Id: I913e67f8ce868d087cb7db55e319791fc384f12f

--- ril/Android.mk
+++ ril/Android.mk
@@ -22,6 +22,23 @@ ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(BOARD_PROVIDES_LIBRIL),true)
 ifneq ($(filter m7450 mdm9x35 ss333 tss310 xmm6260 xmm6262 xmm6360 xmm7260,$(BOARD_MODEM_TYPE)),)
 include $(RIL_PATH)/libril/Android.mk
+
+# RIL.java overwrite
+BOARD_RIL_CLASS := $(RIL_PATH)/rilj
+
+ifneq ($(filter m7450 mdm9x35 ss333 tss310 xmm7260,$(BOARD_MODEM_TYPE)),)
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.ril.samsung_nextgen_modem=true
+endif
+
+ifeq ($(BOARD_MODEM_NEEDS_VIDEO_CALL_FIELD), true)
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.ril.needs_videocall_field=true
+endif
+
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.telephony.ril_class=SamsungExynosRIL
+
 endif
 endif
 
