e3999d3ee9031f234dd33233da6eb236fda0ffa3
Dominggoes Isakh <drjisakh@gmail.com>
2017-08-08 16:43:53 +0200

    liblights: Add led-blink support for BLN
    Change-Id: I0f112abdae3dcd9b94520e41e2e8f4fb956300cf

--- liblights/lights.c
+++ liblights/lights.c
@@ -308,6 +308,26 @@ static int set_light_bln_notifications(struct light_device_t *dev __unused,
 {
     int err = 0;
 
+    if (hw_components & COMPONENT_LED) {
+        ALOGD("%s: BLN LED_BLINK color=0x%010x, fM=%d, fOnMS=%d, fOffMs=%d.", __func__,
+            state->color, state->flashMode, state->flashOnMS, state->flashOffMS);
+
+        struct led_config led;
+        switch (state->flashMode) {
+            case LIGHT_FLASH_TIMED:
+            case LIGHT_FLASH_HARDWARE:
+                led.delay_on  = state->flashOnMS;
+                led.delay_off = state->flashOffMS;
+                break;
+            default:
+                /* Set LED to a solid color, spec is unclear on the exact behavior here. */
+                led.delay_on = led.delay_off = 0;
+                break;
+        }
+        led.color = LED_BRIGHTNESS_NOTIFICATION;
+        write_leds(&led);
+    }
+
     pthread_mutex_lock(&g_lock);
     err = write_str(LED_BLN_NODE, state->color & COLOR_MASK ? "1" : "0");
     pthread_mutex_unlock(&g_lock);
