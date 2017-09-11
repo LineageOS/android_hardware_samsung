0231cc5d3dc6a3312c45d66eef36ea8c0f1e073e
Matt Mower <mowerm@gmail.com>
2017-09-11 08:39:55 +0000

    libril: Force RADIO_TECH_1xRTT on lteOnCdmaDevice
    Ensure CDMA variants have CDMALTEPhone objects setup in frameworks
    instead of GSM. This resolves issues with *- dialing and +1 dialing on
    Verizon Wireless, for example. Credit to Nolan Burchfield for
    identifying the issue.
    
    Change-Id: Ib5188bbf1bbed290962c0e236af96c2f1639f954

--- ril/libril/ril.cpp
+++ ril/libril/ril.cpp
@@ -1780,6 +1780,9 @@ static void dispatchDataCall(Parcel& p, RequestInfo *pRI) {
 // When all RILs handle this request, this function can be removed and
 // the request can be sent directly to the RIL using dispatchVoid.
 static void dispatchVoiceRadioTech(Parcel& p, RequestInfo *pRI) {
+
+    char lteOnCdmaDevice[PROPERTY_VALUE_MAX];
+
     RIL_RadioState state = CALL_ONSTATEREQUEST((RIL_SOCKET_ID)pRI->socket_id);
 
     if ((RADIO_STATE_UNAVAILABLE == state) || (RADIO_STATE_OFF == state)) {
@@ -1788,7 +1791,14 @@ static void dispatchVoiceRadioTech(Parcel& p, RequestInfo *pRI) {
 
     // RILs that support RADIO_STATE_ON should support this request.
     if (RADIO_STATE_ON == state) {
-        dispatchVoid(p, pRI);
+        if (property_get_bool("telephony.lteOnCdmaDevice", false)) {
+            RLOGD("dispatchVoiceRadioTech: lteOnCdmaDevice, forcing RADIO_TECH_1xRTT");
+            voiceRadioTech = RADIO_TECH_1xRTT;
+            RIL_onRequestComplete(pRI, RIL_E_SUCCESS, &voiceRadioTech,
+                                  sizeof(int));
+        } else {
+            dispatchVoid(p, pRI);
+        }
         return;
     }
 
