d8e1a1510724e11d2e9f6186169a972a63f21698
Arne Coucheron <arco68@gmail.com>
2017-10-14 08:21:29 +0200

    samsung: doze: Rebrand from cyanogenmod to lineageos
    Change-Id: I619080f46a69a80fe3923cb3df6a676242f36eac

--- doze/src/org/lineageos/settings/device/SamsungDozeActivity.java
+++ doze/src/org/lineageos/settings/device/SamsungDozeActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.cyanogenmod.settings.device;
+package org.lineageos.settings.device;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
