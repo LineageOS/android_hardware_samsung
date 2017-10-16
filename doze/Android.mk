5ce7cfe2f1c16e785518f7e1badddff0179ff83e
Bruno Martins <bgcngm@gmail.com>
2017-10-16 02:33:15 +0200

    samsung: doze: Build with AAPT2
    Change-Id: Id0a60b490a504010dc8b3f16aa495044719a4062

--- doze/Android.mk
+++ doze/Android.mk
@@ -9,25 +9,22 @@ LOCAL_PACKAGE_NAME := SamsungDoze
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v14-preference \
     android-support-v7-appcompat \
     android-support-v7-preference \
-    android-support-v7-recyclerview \
+    android-support-v7-recyclerview
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
     org.lineageos.platform.internal
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    $(LOCAL_PATH)/../../../../packages/resources/devicesettings/res \
-    frameworks/support/v14/preference/res \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/preference/res \
-    frameworks/support/v7/recyclerview/res
-
-LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v14.preference:android.support.v7.appcompat:android.support.v7.preference:android.support.v7.recyclerview
+    $(TOP)/packages/resources/devicesettings/res
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
