b057e8b50dbbb9e2a416b6b8fe8f05447508ca1f
Christopher N. Hesse <raymanfx@gmail.com>
2018-03-06 20:16:00 +0100

    audio: Update mixer knobs after re-applying output route
    Change-Id: I35fbcc03e9666e579134512b443610e546c31441

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -1021,6 +1021,7 @@ int disable_snd_device(struct audio_device *adev,
                  */
                 out_snd_device_name = get_snd_device_name(out_uc_info->out_snd_device);
                 audio_route_apply_path(mixer_card->audio_route, out_snd_device_name);
+                update_mixer = true;
             }
             if (update_mixer) {
                 audio_route_update_mixer(mixer_card->audio_route);
