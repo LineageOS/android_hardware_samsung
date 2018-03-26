5869cd357e9b0bd9dffe70ac207cda4ec266b73c
Victor Lourme <l0w@protonmail.ch>
2018-03-26 19:36:07 +0200

    audio: return EINVAL to avoid get_presentation_position spamming logs
    Change-Id: I32ec0c5651395c7af3dc4b80d25204bf0fcc1977

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -3303,7 +3303,7 @@ static int out_get_presentation_position(const struct audio_stream_out *stream,
                                    uint64_t *frames, struct timespec *timestamp)
 {
     struct stream_out *out = (struct stream_out *)stream;
-    int ret = -1;
+    int ret = -EINVAL;
 
     lock_output_stream(out);
 
@@ -3342,7 +3342,6 @@ static int out_get_presentation_position(const struct audio_stream_out *stream,
                             ret = 0;
                             goto done;
                         }
-                        ret = -1;
                     }
                 }
             }
