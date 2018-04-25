8ecde4feb29e655d92e3a93644b7b01fa238302a
stenkinevgeniy <stenkinevgeniy@gmail.com>
2018-04-25 08:04:48 +0000

    Audio: Rewrite Audio HAL.
    * remove mixer lists
    * remove pcm_dev_lists
    * use proper pcm_config for each hw pcm device
    * rewrite enable_snd_device and disable_snd_device
    * move mixer path to device tree header
    * use separate stream for voice calls
    * add check_and_route_usecases.
      this function is to make sure that all the usecases that are active on
      the hardware  are always routed to any one device that is
      handled by the hardware.
    
    This patch need updating device audio header (if device use a separate header) - samsung_audio.h
    
    Change-Id: Ibea80e5d2897181e3df3074866dfdb8563a19ce1

--- audio/audio_hw.h
+++ audio/audio_hw.h
@@ -235,33 +235,14 @@ struct offload_cmd {
     int             data[];
 };
 
-struct pcm_device_profile {
-    struct pcm_config config;
-    int               card;
-    int               id;
-    usecase_type_t    type;
-    audio_devices_t   devices;
-};
-
-struct pcm_device {
-    struct listnode            stream_list_node;
-    struct pcm_device_profile* pcm_profile;
-    struct pcm*                pcm;
-    int                        status;
-    /* TODO: remove resampler if possible when AudioFlinger supports downsampling from 48 to 8 */
-    struct resampler_itfe*     resampler;
-    int16_t*                   res_buffer;
-    size_t                     res_byte_count;
-    int                        sound_trigger_handle;
-};
-
 struct stream_out {
     struct audio_stream_out     stream;
     pthread_mutex_t             lock; /* see note below on mutex acquisition order */
     pthread_mutex_t             pre_lock; /* acquire before lock to avoid DOS by playback thread */
     pthread_cond_t              cond;
     struct pcm_config           config;
-    struct listnode             pcm_dev_list;
+    int                         pcm_device_id;
+    struct pcm                  *pcm;
     struct compr_config         compr_config;
     struct compress*            compr;
     int                         standby;
@@ -312,7 +293,8 @@ struct stream_in {
     pthread_mutex_t                     pre_lock; /* acquire before lock to avoid DOS by
                                                      capture thread */
     struct pcm_config                   config;
-    struct listnode                     pcm_dev_list;
+    struct pcm                          *pcm;
+    int                                 pcm_device_id;
     int                                 standby;
     audio_source_t                      source;
     audio_devices_t                     devices;
@@ -363,15 +345,6 @@ struct stream_in {
                                                         entering standby */
 };
 
-struct mixer_card {
-    struct listnode     adev_list_node;
-    struct listnode     uc_list_node[AUDIO_USECASE_MAX];
-    int                 card;
-    struct mixer*       mixer;
-    struct audio_route* audio_route;
-    struct timespec     dsp_poweroff_time;
-};
-
 struct audio_usecase {
     struct listnode         adev_list_node;
     audio_usecase_t         id;
@@ -380,7 +353,6 @@ struct audio_usecase {
     snd_device_t            out_snd_device;
     snd_device_t            in_snd_device;
     struct audio_stream*    stream;
-    struct listnode         mixer_list;
 };
 
 struct voice_data {
@@ -394,10 +366,12 @@ struct voice_data {
 struct audio_device {
     struct audio_hw_device  device;
     pthread_mutex_t         lock; /* see note below on mutex acquisition order */
-    struct listnode         mixer_list;
+    struct mixer            *mixer;
+    struct audio_route      *audio_route;
     audio_mode_t            mode;
     struct stream_in*       active_input;
     struct stream_out*      primary_output;
+    struct stream_out*      current_call_output;
     bool                    mic_mute;
     bool                    screen_off;
 
