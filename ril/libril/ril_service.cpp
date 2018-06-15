5dfea70d8314e54bfdca462cc03df943607fca8a
Kevin F. Haggerty <haggertk@lineageos.org>
2018-06-15 14:53:38 -0600

    libril: Build non-inlined android_atomic methods
    * All of the libsec-ril*.so libraries reference these symbols, but
      Google finally dropped the non-inlined versions from libcutils with
      Android 8.0. This could be handled with shims in numerous device
      trees, but shim semantics and implementation aren't exactly stable
      and we can handle it more cleanly here in one place.
    * See LineageOS/android_system_core@103e8f560
    
    Change-Id: I787372b739f3ace0d9cbbc33e4bffafa6876665e

--- ril/libril/ril_service.cpp
+++ ril/libril/ril_service.cpp
@@ -27,6 +27,11 @@
 #include <inttypes.h>
 #include <cutils/properties.h>
 
+#define ANDROID_ATOMIC_INLINE
+extern "C" {
+#include <cutils/atomic.h>
+}
+
 #define INVALID_HEX_CHAR 16
 
 // Enable verbose logging
