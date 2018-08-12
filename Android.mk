9d45896af6026e159757d770a3e7aeff4900e8d0
Stricted <info@stricted.net>
2018-08-12 22:01:19 +0200

    DNM: exclude AdvancedDisplay
    Change-Id: I2fd31b586610a1ed70b74a6ff14e8bb8a8616270

--- Android.mk
+++ Android.mk
@@ -36,7 +36,7 @@ include $(SAM_ROOT)/wifiloader/Android.mk
 endif
 
 ifeq ($(BOARD_VENDOR),samsung)
-include $(SAM_ROOT)/AdvancedDisplay/Android.mk
+#include $(SAM_ROOT)/AdvancedDisplay/Android.mk
 include $(SAM_ROOT)/audio/Android.mk
 include $(SAM_ROOT)/consumerir/Android.mk
 include $(SAM_ROOT)/dtbhtool/Android.mk
