d0e208008ccc1385c0ca61fe0bd40e7f92fcb9e8
LuK1337 <priv.luk@gmail.com>
2018-08-25 12:31:48 +0200

    AdvancedDisplay: Migrate to AAPT2
    Change-Id: I59dabea4484dd1f1e5c57ae2ac37b9939d2c875d

--- AdvancedDisplay/Android.mk
+++ AdvancedDisplay/Android.mk
@@ -8,31 +8,24 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := AdvancedDisplay
 LOCAL_CERTIFICATE := platform
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v4 \
     android-support-v7-appcompat \
     android-support-v7-preference \
     android-support-v7-recyclerview \
     android-support-v13 \
-    android-support-v14-preference \
-    org.lineageos.platform.internal
+    android-support-v14-preference
 
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.lineageos.platform.internal
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    $(TOP)/packages/resources/devicesettings/res \
-    frameworks/support/v14/preference/res \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/preference/res \
-    frameworks/support/v7/recyclerview/res
+    $(TOP)/packages/resources/devicesettings/res
 
-LOCAL_AAPT_FLAGS := \
-    --auto-add-overlay \
-    --extra-packages android.support.v14.preference \
-    --extra-packages android.support.v7.appcompat \
-    --extra-packages android.support.v7.preference \
-    --extra-packages android.support.v7.recyclerview
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
 
