172a583cca12e6a23badd8221286af713d54bb1a
Jan Altensen <info@stricted.net>
2019-01-20 02:40:07 +0100

    hidl: livedisplay: Add binderized service implementation
    * Change default ::implementation namespace to ::samsung
    * Fill in required methods for used impls
    * Cleanup passthrough code for used impls
    * Remove unused impls
    * Add and setup binderized service
    * Remove lineagehw impls
    
    Change-Id: I545a7c0ac8bf4fce04da73a0d39d4ac1938496f2

--- lineagehw/hidl/livedisplay/DisplayColorCalibration.h
+++ lineagehw/hidl/livedisplay/DisplayColorCalibration.h
@@ -25,7 +25,7 @@ namespace vendor {
 namespace lineage {
 namespace livedisplay {
 namespace V2_0 {
-namespace implementation {
+namespace samsung {
 
 using ::android::hardware::hidl_array;
 using ::android::hardware::hidl_memory;
@@ -35,21 +35,21 @@ using ::android::hardware::Return;
 using ::android::hardware::Void;
 using ::android::sp;
 
-struct DisplayColorCalibration : public IDisplayColorCalibration {
+class DisplayColorCalibration : public IDisplayColorCalibration {
+  public:
+    bool isSupported();
+
     // Methods from ::vendor::lineage::livedisplay::V2_0::IDisplayColorCalibration follow.
     Return<int32_t> getMaxValue() override;
     Return<int32_t> getMinValue() override;
-    Return<void> getCalibration(getCalibration_cb _hidl_cb) override;
+    Return<void> getCalibration(getCalibration_cb resultCb) override;
     Return<bool> setCalibration(const hidl_vec<int32_t>& rgb) override;
 
     // Methods from ::android::hidl::base::V1_0::IBase follow.
 
 };
 
-// FIXME: most likely delete, this is only for passthrough implementations
-// extern "C" IDisplayColorCalibration* HIDL_FETCH_IDisplayColorCalibration(const char* name);
-
-}  // namespace implementation
+}  // namespace samsung
 }  // namespace V2_0
 }  // namespace livedisplay
 }  // namespace lineage
