802fd001d478d9f057599b4f9d49fbdddd5d6405
Nolen Johnson <johnsonnolen@gmail.com>
2019-08-07 23:56:48 -0400

    mkbootimg: Add Loki support
    * I want my target-files built images to be Loki'd if the device
      needs Loki.
    * With this, devices that rely on Loki can ship Lineage Recovery
      officially.
    * Import loki_tool from: https://github.com/Stricted/android_external_loki.
    
    Change-Id: I45ef363e05566268c8f24f7e8939a2d785478fbe

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -60,10 +60,19 @@ dtimage: $(INSTALLED_DTIMAGE_TARGET)
 endif
 endif
 
+ifeq ($(strip $(TAGET_NEEDS_LOKI)),true)
+LOKI_TOOL := loki_tool
+else
+LOKI_TOOL := echo
+TARGET_LOKI_ABOOT_IMAGE :=
+endif
+
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIMAGE_EXTRA_DEPS)
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) echo -n "SEANDROIDENFORCE" >> $@
+	$(hide) $(LOKI_TOOL) patch boot $(TARGET_LOKI_ABOOT_IMAGE) $@ $@.lok
+	$(hide) cp $@.lok $@ || true
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 	@echo "Made boot image: $@"
 
@@ -71,5 +80,7 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_k
 	@echo "----- Making recovery image ------"
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@ --id > $(RECOVERYIMAGE_ID_FILE)
 	$(hide) echo -n "SEANDROIDENFORCE" >> $@
+	$(hide) $(LOKI_TOOL) patch recovery $(TARGET_LOKI_ABOOT_IMAGE) $@ $@.lok
+	$(hide) cp $@.lok $@ || true
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo "Made recovery image: $@"
