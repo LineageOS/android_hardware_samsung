cc5c17ec8d500fe72b6b472789bc6891ccb38365
Jan Altensen <info@stricted.net>
2019-09-07 21:10:20 +0200

    samsung: AdvancedDisplay: Migrate to androidx
    Change-Id: I9f6f936120e6eac06c78525f3692207ff58c991f

--- AdvancedDisplay/Android.mk
+++ AdvancedDisplay/Android.mk
@@ -12,12 +12,12 @@ LOCAL_CERTIFICATE := platform
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v4 \
-    android-support-v7-appcompat \
-    android-support-v7-preference \
-    android-support-v7-recyclerview \
-    android-support-v13 \
-    android-support-v14-preference
+    androidx.appcompat_appcompat \
+    androidx.preference_preference \
+    androidx.recyclerview_recyclerview \
+    androidx.legacy_legacy-preference-v14 \
+    androidx.legacy_legacy-support-v4 \
+    androidx.legacy_legacy-support-v13
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     org.lineageos.platform.internal
