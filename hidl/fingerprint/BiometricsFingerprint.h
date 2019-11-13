7d07356f57763d4ff30119d54b93bf8cfe48f4cf
Willi Ye <williye97@gmail.com>
2019-11-13 01:38:23 +0100

    samsung: hidl: fingerprint: Add inscreen support
    Change-Id: I18e1835fd268d8b819f874029a88065d8be167bf

--- hidl/fingerprint/BiometricsFingerprint.h
+++ hidl/fingerprint/BiometricsFingerprint.h
@@ -46,9 +46,10 @@ struct BiometricsFingerprint : public IBiometricsFingerprint {
     // Method to wrap legacy HAL with BiometricsFingerprint class
     static IBiometricsFingerprint* getInstance();
 
-    // Methods from ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint follow.
+    // Methods from ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint
+    // follow.
     Return<uint64_t> setNotify(
-        const sp<IBiometricsFingerprintClientCallback>& clientCallback) override;
+            const sp<IBiometricsFingerprintClientCallback>& clientCallback) override;
     Return<uint64_t> preEnroll() override;
     Return<RequestStatus> enroll(const hidl_array<uint8_t, 69>& hat, uint32_t gid,
                                  uint32_t timeoutSec) override;
@@ -63,7 +64,7 @@ struct BiometricsFingerprint : public IBiometricsFingerprint {
   private:
     bool openHal();
     static void notify(
-        const fingerprint_msg_t* msg); /* Static callback for legacy HAL implementation */
+            const fingerprint_msg_t* msg); /* Static callback for legacy HAL implementation */
     static Return<RequestStatus> ErrorFilter(int32_t error);
     static FingerprintError VendorErrorFilter(int32_t error, int32_t* vendorCode);
     static FingerprintAcquiredInfo VendorAcquiredFilter(int32_t error, int32_t* vendorCode);
