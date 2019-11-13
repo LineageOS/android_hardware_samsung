f6a8f6e6ecfe1939871082adcfcbdac64248bc03
Willi Ye <williye97@gmail.com>
2019-11-13 01:38:23 +0100

    samsung: hidl: fingerprint: Add inscreen support
    Change-Id: I18e1835fd268d8b819f874029a88065d8be167bf

--- hidl/fingerprint/inscreen/Android.mk
+++ hidl/fingerprint/inscreen/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,8 +19,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    BiometricsFingerprint.cpp \
+    FingerprintInscreen.cpp \
     service.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 
 LOCAL_SHARED_LIBRARIES := \
     libbase \
@@ -28,28 +30,15 @@ LOCAL_SHARED_LIBRARIES := \
     libhidlbase \
     libhidltransport \
     liblog \
+    libhwbinder \
     libutils \
-    android.hardware.biometrics.fingerprint@2.1
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0
 
-ifeq ($(TARGET_SEC_FP_CALL_NOTIFY_ON_CANCEL),true)
-    LOCAL_CFLAGS += -DCALL_NOTIFY_ON_CANCEL
-endif
-
-ifeq ($(TARGET_SEC_FP_USES_PERCENTAGE_SAMPLES),true)
-    LOCAL_CFLAGS += -DUSES_PERCENTAGE_SAMPLES
-endif
-
-ifeq ($(TARGET_SEC_FP_CALL_CANCEL_ON_ENROLL_COMPLETION),true)
-    LOCAL_CFLAGS += -DCALL_CANCEL_ON_ENROLL_COMPLETION
-endif
-
-LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.1-service.samsung
-LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.1-service.samsung.rc
+LOCAL_MODULE := vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung
+LOCAL_INIT_RC := vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung.rc
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_OWNER := samsung
 LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_EXECUTABLE)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
