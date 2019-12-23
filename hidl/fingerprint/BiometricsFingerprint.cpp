29aa9b74ca777a84f6de911f32171208ea4039e7
Danny Wood <danwood76@gmail.com>
2019-12-23 11:37:10 +0000

    samsung: hidl: correct USES_PERCENTAGE_SAMPLES behaviour as previously it did nothing
    This fixes fingerprint on the SM-A510F and possibly other older Samsung devices when using the new
    HIDL implementation
    
    Change-Id: Ie8883d3d628e924a9b181b08681388cd05e5f325

--- hidl/fingerprint/BiometricsFingerprint.cpp
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -288,19 +288,20 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t* msg) {
             }
         } break;
         case FINGERPRINT_TEMPLATE_ENROLLING:
+#ifdef USES_PERCENTAGE_SAMPLES
+            {
+                fingerprint_msg_t *new_msg = (fingerprint_msg_t *)msg;
+                new_msg->data.enroll.samples_remaining = 100 - msg->data.enroll.samples_remaining;
+                msg = new_msg;
+            }
+#endif
             LOG(DEBUG) << "onEnrollResult(fid=" << msg->data.enroll.finger.fid
                        << ", gid=" << msg->data.enroll.finger.gid
                        << ", rem=" << msg->data.enroll.samples_remaining << ")";
-            if (thisPtr->mClientCallback
+            if (!thisPtr->mClientCallback
                     ->onEnrollResult(devId, msg->data.enroll.finger.fid,
                                      msg->data.enroll.finger.gid, msg->data.enroll.samples_remaining)
                     .isOk()) {
-#ifdef USES_PERCENTAGE_SAMPLES
-                fingerprint_msg_t* newMsg = (fingerprint_msg_t*)msg;
-                newMsg->data.enroll.samples_remaining = 100 - msg->data.enroll.samples_remaining;
-                msg = newMsg;
-#endif
-            } else {
                 LOG(ERROR) << "failed to invoke fingerprint onEnrollResult callback";
             }
             break;
