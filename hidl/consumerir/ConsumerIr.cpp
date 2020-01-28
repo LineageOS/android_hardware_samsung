bde3a83a2b7fb50fc869d2d7600aeef11f289b8a
Paul Keith <javelinanddart@gmail.com>
2020-01-27 20:19:30 -0600

    hidl: consumerir: Setup boilerplate code for HIDL hal
    Change-Id: I5347b6b7d2b12f4f5ae0990503e50f22850746fb

--- hidl/consumerir/ConsumerIr.cpp
+++ hidl/consumerir/ConsumerIr.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "ConsumerIr.h"
 
 namespace android {
@@ -17,13 +33,6 @@ Return<void> ConsumerIr::getCarrierFreqs(getCarrierFreqs_cb _hidl_cb) {
     return Void();
 }
 
-
-// Methods from ::android::hidl::base::V1_0::IBase follow.
-
-//IConsumerIr* HIDL_FETCH_IConsumerIr(const char* /* name */) {
-    //return new ConsumerIr();
-//}
-//
 }  // namespace implementation
 }  // namespace V1_0
 }  // namespace ir
