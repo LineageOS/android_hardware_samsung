133f482dcd34a8f2f85e7934448d1f7ee61e40a2
Paul Keith <javelinanddart@gmail.com>
2020-01-27 20:19:30 -0600

    hidl: consumerir: Setup boilerplate code for HIDL hal
    Change-Id: I5347b6b7d2b12f4f5ae0990503e50f22850746fb

--- hidl/consumerir/ConsumerIr.h
+++ hidl/consumerir/ConsumerIr.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef ANDROID_HARDWARE_IR_V1_0_CONSUMERIR_H
 #define ANDROID_HARDWARE_IR_V1_0_CONSUMERIR_H
 
@@ -11,25 +27,15 @@ namespace ir {
 namespace V1_0 {
 namespace implementation {
 
-using ::android::hardware::hidl_array;
-using ::android::hardware::hidl_memory;
-using ::android::hardware::hidl_string;
-using ::android::hardware::hidl_vec;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
-using ::android::sp;
 
-struct ConsumerIr : public IConsumerIr {
+class ConsumerIr : public IConsumerIr {
+  public:
     // Methods from ::android::hardware::ir::V1_0::IConsumerIr follow.
     Return<bool> transmit(int32_t carrierFreq, const hidl_vec<int32_t>& pattern) override;
     Return<void> getCarrierFreqs(getCarrierFreqs_cb _hidl_cb) override;
-
-    // Methods from ::android::hidl::base::V1_0::IBase follow.
-
 };
-
-// FIXME: most likely delete, this is only for passthrough implementations
-// extern "C" IConsumerIr* HIDL_FETCH_IConsumerIr(const char* name);
 
 }  // namespace implementation
 }  // namespace V1_0
