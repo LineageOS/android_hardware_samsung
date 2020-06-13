f1e23cabb6c3e1ac93aeefc7469bcf9bea7c29cc
Jesse Chan <jc@lineageos.org>
2020-06-13 11:22:14 +0800

    power-libperfmgr: adapt and rebrand for hardware/samsung
    Change-Id: I8aa9a712ac7cbc6085928f7cdb9e6ada8ed5e3fe

--- hidl/power-libperfmgr/display-helper.cpp
+++ hidl/power-libperfmgr/display-helper.cpp
@@ -15,7 +15,7 @@
  */
 
 #define LOG_NIDEBUG 0
-#define LOG_TAG "android.hardware.power@1.3-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.samsung-libperfmgr"
 
 #include <dlfcn.h>
 #include <errno.h>
