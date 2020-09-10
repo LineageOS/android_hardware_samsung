f78616bab30b92714873d59b07a14f2e41af5894
Jan Altensen <info@stricted.net>
2020-09-11 00:08:00 +0200

    samsung: convert dtbhtoolExynos to blueprint
    Change-Id: I0f9aff67e76ac47f792e92fca4802b50f893d37c

--- Android.mk
+++ Android.mk
@@ -39,7 +39,6 @@ ifeq ($(BOARD_VENDOR),samsung)
 include $(SAM_ROOT)/AdvancedDisplay/Android.mk
 include $(SAM_ROOT)/audio/Android.mk
 include $(SAM_ROOT)/doze/Android.mk
-include $(SAM_ROOT)/dtbhtool/Android.mk
 include $(SAM_ROOT)/hidl/Android.mk
 include $(SAM_ROOT)/modemloader/Android.mk
 include $(SAM_ROOT)/ril/Android.mk
