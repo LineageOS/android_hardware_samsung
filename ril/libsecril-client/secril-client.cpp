1ddff4f7c990e76050fcdec5aebcd65e3853abe0
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-11-23 21:52:33 +0100

    libsecril-client: fix socket name on new libsecril blobs
    Change-Id: Iab5d07f2301d33216bbdf3e18f844522e32fadce

--- ril/libsecril-client/secril-client.cpp
+++ ril/libsecril-client/secril-client.cpp
@@ -35,9 +35,9 @@ namespace android {
 // Defines
 //---------------------------------------------------------------------------
 #define RILD_PORT               7777
-#define MULTI_CLIENT_SOCKET_NAME "Multiclient"
+#define MULTI_CLIENT_SOCKET_NAME "VND_Multiclient"
 #define MULTI_CLIENT_Q_SOCKET_NAME "QMulticlient"
-#define MULTI_CLIENT_SOCKET_NAME_2 "Multiclient2"
+#define MULTI_CLIENT_SOCKET_NAME_2 "VND_Multiclient2"
 
 #define MAX_COMMAND_BYTES       (8 * 1024)
 #define REQ_POOL_SIZE           32
