0ded5e1f02f544e63ba01bdcd9c18a1e8a7fda58
Bruno Martins <bgcngm@gmail.com>
2020-12-26 14:55:22 +0000

    samsung: doze: Apply a major refactor
     * Introduce a main switch to actually enable/disable doze globally
     * Move preferences handling out of SamsungDozeService class and
       create Utils class to hold all helper methods.
    
    Change-Id: Idb40650296d50352b4a985d2068f14cda6cb4aaf

---
+++ doze/src/org/lineageos/settings/doze/Utils.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017-2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.doze;
+
+import static android.provider.Settings.Secure.DOZE_ENABLED;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.Log;
+
+import androidx.preference.PreferenceManager;
+
+public final class Utils {
+
+    private static final String TAG = "DozeUtils";
+    private static final boolean DEBUG = false;
+
+    protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
+    protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
+
+    protected static void startService(Context context) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        context.startServiceAsUser(new Intent(context, SamsungDozeService.class),
+                UserHandle.CURRENT);
+    }
+
+    protected static void stopService(Context context) {
+        if (DEBUG) Log.d(TAG, "Stopping service");
+        context.stopServiceAsUser(new Intent(context, SamsungDozeService.class),
+                UserHandle.CURRENT);
+    }
+
+    protected static void checkDozeService(Context context) {
+        if (isDozeEnabled(context) && isAnyGestureEnabled(context)) {
+            startService(context);
+        } else {
+            stopService(context);
+        }
+    }
+
+    protected static boolean isDozeEnabled(Context context) {
+        return Settings.Secure.getInt(context.getContentResolver(),
+                DOZE_ENABLED, 1) != 0;
+    }
+
+    protected static boolean enableDoze(Context context, boolean enable) {
+        return Settings.Secure.putInt(context.getContentResolver(),
+                DOZE_ENABLED, enable ? 1 : 0);
+    }
+
+    protected static void enableGesture(Context context, String gesture, boolean enable) {
+        PreferenceManager.getDefaultSharedPreferences(context).edit()
+                .putBoolean(gesture, enable).apply();
+    }
+
+    protected static boolean isAnyGestureEnabled(Context context) {
+        return isHandwaveGestureEnabled(context) || isPocketGestureEnabled(context);
+    }
+
+    protected static boolean isHandwaveGestureEnabled(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(GESTURE_HAND_WAVE_KEY, false);
+    }
+
+    protected static boolean isPocketGestureEnabled(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(GESTURE_POCKET_KEY, false);
+    }
+}
