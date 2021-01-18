84e79a048f7fcff35ac34cfec09de8538b8d78ce
Bruno Martins <bgcngm@gmail.com>
2021-01-18 22:31:07 +0000

    fastcharge: Make user setting persistent
    Change-Id: I4ff051d6f9685e94022eab32035bfe86a65264b8

--- hidl/fastcharge/include/samsung_fastcharge.h
+++ hidl/fastcharge/include/samsung_fastcharge.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The LineageOS Project
+ * Copyright (C) 2020-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 #ifndef SAMSUNG_FASTCHARGE_H
 #define SAMSUNG_FASTCHARGE_H
 
+#define FASTCHARGE_DEFAULT_SETTING true
 #define FASTCHARGE_PATH "/sys/class/sec/switch/afc_disable"
 
 #endif // SAMSUNG_FASTCHARGE_H
