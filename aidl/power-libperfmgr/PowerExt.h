26971dace406ce14d724a0be23efc9d1978ede21
Michael Benedict <michaelbt@live.com>
2021-03-14 12:13:43 +0700

    aidl: power-libperfmgr: Squash some of previous power-libperfmgr commits
    These commits are adapted for aidl counterpart.
    power-libperfmgr: adapt and rebrand for hardware/samsung
    power-libperfmgr: simply wait if idle state node is not present
    power-libperfmgr: remove Google-specific display LPM control
    power-libperfmgr: remove Google-specific camera and audio hints
    power-libperfmgr: Give access to radio group
    power-libperfmgr: initialize powerHAL when boot is completed
    power-libperfmgr: declare override="true" in manifest
    power-libperfmgr: add a Power Hint template with common nodes
    
    Notes:
    * Interactive, DT2W, and Lineage perf hint will be re-implemented outside of this commit.
    
    Signed-off-by: Michael Benedict <michaelbt@live.com>
    Change-Id: I7034cb083bd20ec3a62ec338f5cbfb3e3ba7a0ff

--- aidl/power-libperfmgr/PowerExt.h
+++ aidl/power-libperfmgr/PowerExt.h
@@ -23,8 +23,6 @@
 #include <aidl/google/hardware/power/extension/pixel/BnPowerExt.h>
 #include <perfmgr/HintManager.h>
 
-#include "disp-power/DisplayLowPower.h"
-
 namespace aidl {
 namespace google {
 namespace hardware {
@@ -36,8 +34,8 @@ using ::android::perfmgr::HintManager;
 
 class PowerExt : public ::aidl::google::hardware::power::extension::pixel::BnPowerExt {
   public:
-    PowerExt(std::shared_ptr<HintManager> hm, std::shared_ptr<DisplayLowPower> dlpw)
-        : mHintManager(hm), mDisplayLowPower(dlpw) {}
+    PowerExt(std::shared_ptr<HintManager> hm)
+        : mHintManager(hm) {}
     ndk::ScopedAStatus setMode(const std::string &mode, bool enabled) override;
     ndk::ScopedAStatus isModeSupported(const std::string &mode, bool *_aidl_return) override;
     ndk::ScopedAStatus setBoost(const std::string &boost, int32_t durationMs) override;
@@ -45,7 +43,6 @@ class PowerExt : public ::aidl::google::hardware::power::extension::pixel::BnPow
 
   private:
     std::shared_ptr<HintManager> mHintManager;
-    std::shared_ptr<DisplayLowPower> mDisplayLowPower;
 };
 
 }  // namespace pixel
