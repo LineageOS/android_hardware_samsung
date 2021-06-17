5a509c7bcd746ac8ef03f9487f51022f70dd3aa2
Wei Wang <wvw@google.com>
2021-06-16 21:20:44 -0700

    power-libperfmgr: improve adpf logic
    Cache active state and reduce log spam
    Add value tracing into libperfmgr
    Use adaptive stale timeout based on rate limit
    
    Bug: 191331719
    Bug: 191296994
    Bug: 177493042
    Test: boot
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: I1c1484c9277209bf68bd287ceae83e2b37684c62

--- aidl/power-libperfmgr/Power.h
+++ aidl/power-libperfmgr/Power.h
@@ -56,7 +56,7 @@ class Power : public ::aidl::android::hardware::power::BnPower {
     std::unique_ptr<InteractionHandler> mInteractionHandler;
     std::atomic<bool> mVRModeOn;
     std::atomic<bool> mSustainedPerfModeOn;
-    const int64_t mAdpfRate;
+    const int64_t mAdpfRateNs;
 };
 
 }  // namespace pixel
