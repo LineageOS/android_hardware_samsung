c7f14b17566a8340d4302dcbe309c87d7ed7a29d
Jimmy Shiu <jimmyshiu@google.com>
2021-07-29 23:58:22 +0800

    power-libperfmgr: ADPF: avoid to call close() twice
    To get rid of error logs, avoid to call close() twice.
    
    07-29 17:20:35.341 E powerhal-libperfmgr: Unexpected Error! Failed to
    look up tid:2585 in TidRefCountMap
    07-29 17:20:35.341 E powerhal-libperfmgr: Unexpected Error! Failed to
    look up tid:2586 in TidRefCountMap
    07-29 17:20:35.341 E powerhal-libperfmgr: Unexpected Error! Failed to
    look up tid:2031 in TidRefCountMap
    07-29 17:20:35.341 E powerhal-libperfmgr: Unexpected Error! Failed to
    look up tid:2585 in TidRefCountMap
    
    Bug: 194775170
    Test: build and check log.
    Change-Id: I91adf907b837382f68935b9054e19465a499049c

--- aidl/power-libperfmgr/PowerHintSession.h
+++ aidl/power-libperfmgr/PowerHintSession.h
@@ -108,6 +108,7 @@ class PowerHintSession : public BnPowerHintSession {
     sp<MessageHandler> mPowerManagerHandler;
     std::mutex mLock;
     const nanoseconds kAdpfRate;
+    std::atomic<bool> mSessionClosed = false;
 };
 
 }  // namespace pixel
