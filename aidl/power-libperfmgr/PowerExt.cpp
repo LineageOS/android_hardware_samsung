6ad3a001fe961c1b41476e91ca0979a766dab509
Wei Wang <wvw@google.com>
2021-09-17 15:30:09 -0700

    power-libperfmgr: power: move trace point to libperfmgr.
    Test: systrace
    Bug: 199776250
    Change-Id: I9bb4d5a50faa93e7bc638ef723bdc2662fb63b24
    (cherry picked from commit 16f632b2030141474661a9b2e9664a86a8daad11)

--- aidl/power-libperfmgr/PowerExt.cpp
+++ aidl/power-libperfmgr/PowerExt.cpp
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-#define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 #define LOG_TAG "android.hardware.power-service.samsung.ext-libperfmgr"
 
 #include "PowerExt.h"
@@ -29,7 +28,6 @@
 #include <android-base/strings.h>
 
 #include <utils/Log.h>
-#include <utils/Trace.h>
 
 namespace aidl {
 namespace google {
@@ -40,7 +38,6 @@ namespace pixel {
 
 ndk::ScopedAStatus PowerExt::setMode(const std::string &mode, bool enabled) {
     LOG(DEBUG) << "PowerExt setMode: " << mode << " to: " << enabled;
-    ATRACE_INT(mode.c_str(), enabled);
 
     if (enabled) {
         mHintManager->DoHint(mode);
@@ -61,7 +58,6 @@ ndk::ScopedAStatus PowerExt::isModeSupported(const std::string &mode, bool *_aid
 
 ndk::ScopedAStatus PowerExt::setBoost(const std::string &boost, int32_t durationMs) {
     LOG(DEBUG) << "PowerExt setBoost: " << boost << " duration: " << durationMs;
-    ATRACE_INT(boost.c_str(), durationMs);
 
     if (durationMs > 0) {
         mHintManager->DoHint(boost, std::chrono::milliseconds(durationMs));
