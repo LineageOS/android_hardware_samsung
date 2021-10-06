3b65a863d704b52ffb93d9f68ae5b9f4433b4355
TheScarastic <warabhishek@gmail.com>
2021-10-06 07:25:13 +0000

    AdvancedDisplay: Adapt to S style
     * Use Theme.SubSettingsBase for theme
    
    Original Change-Id: Ic879ce61d83a33da12829008a4336c8a23230d63
    Change-Id: Ib16512c898f7419cd40ab71985945dc5636c26de
    
    Change-Id: I603bde2d7e3f6d3a1fbae37ca0300100917fa7be
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- AdvancedDisplay/src/org/lineageos/settings/device/AdvancedDisplayActivity.java
+++ AdvancedDisplay/src/org/lineageos/settings/device/AdvancedDisplayActivity.java
@@ -17,14 +17,16 @@
 package org.lineageos.settings.device;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 
-public class AdvancedDisplayActivity extends PreferenceActivity {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class AdvancedDisplayActivity extends CollapsingToolbarBaseActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
                 new AdvancedDisplayFragment()).commit();
     }
 }
