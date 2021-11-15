61f341dca622c3dfb41e9302bce6f0a41d5f97ed
roynatech2544 <whiteshell2544@naver.com>
2021-11-15 22:10:49 +0900

    Implement SamsungDap
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Signed-off-by: roynatech2544 <whiteshell2544@naver.com>
    Change-Id: I7f52714bdf7d94c9fb1c2d645839d54fd09d6bbe

---
+++ dap/src/org/lineageos/dap/DolbyTile.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.dap
+
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+
+import androidx.preference.PreferenceManager
+
+import org.lineageos.dap.DolbyFragment.Companion.PREF_DOLBY_ENABLE
+
+class DolbyTile : TileService() {
+    private var isEnabled = false
+        set(value) {
+            field = value
+            qsTile.state = if (value) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+            qsTile.updateTile()
+        }
+
+    override fun onStartListening() {
+        super.onStartListening()
+        isEnabled = DolbyCore.isEnabled()
+    }
+
+    override fun onClick() {
+        isEnabled = !isEnabled
+        DolbyCore.setEnabled(isEnabled)
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .edit()
+                .putBoolean(PREF_DOLBY_ENABLE, isEnabled)
+                .commit()
+    }
+}
