8e764a8225539dd56553d04617ba872f00e332cb
Bruno Martins <bgcngm@gmail.com>
2022-01-13 20:43:09 +0000

    hidl: livedisplay: Use pragma once directive everywhere
    Change-Id: Ic5ec76210a7d8697945561b2d150f0c1f778dc5c

--- hidl/livedisplay/DisplayColorCalibration.h
+++ hidl/livedisplay/DisplayColorCalibration.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2019-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,7 @@
  * limitations under the License.
  */
 
-#ifndef VENDOR_LINEAGE_LIVEDISPLAY_V2_0_DISPLAYCOLORCALIBRATION_H
-#define VENDOR_LINEAGE_LIVEDISPLAY_V2_0_DISPLAYCOLORCALIBRATION_H
+#pragma once
 
 #include <vendor/lineage/livedisplay/2.0/IDisplayColorCalibration.h>
 
@@ -47,5 +46,3 @@ class DisplayColorCalibration : public IDisplayColorCalibration {
 }  // namespace livedisplay
 }  // namespace lineage
 }  // namespace vendor
-
-#endif  // VENDOR_LINEAGE_LIVEDISPLAY_V2_0_DISPLAYCOLORCALIBRATION_H
