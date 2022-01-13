d54778c08a2cc9d00321847b4110370918ba60b8
Bruno Martins <bgcngm@gmail.com>
2022-01-13 20:40:41 +0000

    hidl: fingerprint: Use pragma once
    Change-Id: Id3d0a28a94f52a8f9d2c72fe41e633761557951f

--- hidl/fingerprint/BiometricsFingerprint.h
+++ hidl/fingerprint/BiometricsFingerprint.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2019-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,7 @@
  * limitations under the License.
  */
 
-#ifndef ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_3_BIOMETRICSFINGERPRINT_H
-#define ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_3_BIOMETRICSFINGERPRINT_H
+#pragma once
 
 #include <chrono>
 #include <thread>
@@ -118,5 +117,3 @@ struct BiometricsFingerprint : public IBiometricsFingerprint {
 }  // namespace biometrics
 }  // namespace hardware
 }  // namespace android
-
-#endif  // ANDROID_HARDWARE_BIOMETRICS_FINGERPRINT_V2_3_BIOMETRICSFINGERPRINT_H
