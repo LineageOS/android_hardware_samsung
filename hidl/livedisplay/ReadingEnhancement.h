a921d5315c9482bd6b33f4664a6f40e7a2f7ef3c
Bruno Martins <bgcngm@gmail.com>
2022-01-13 20:43:09 +0000

    hidl: livedisplay: Use pragma once directive everywhere
    Change-Id: Ic5ec76210a7d8697945561b2d150f0c1f778dc5c

--- hidl/livedisplay/ReadingEnhancement.h
+++ hidl/livedisplay/ReadingEnhancement.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2019-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,7 @@
  * limitations under the License.
  */
 
-#ifndef VENDOR_LINEAGE_LIVEDISPLAY_V2_0_READINGENHANCEMENT_H
-#define VENDOR_LINEAGE_LIVEDISPLAY_V2_0_READINGENHANCEMENT_H
+#pragma once
 
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
@@ -51,5 +50,3 @@ class ReadingEnhancement : public IReadingEnhancement {
 }  // namespace livedisplay
 }  // namespace lineage
 }  // namespace vendor
-
-#endif  // VENDOR_LINEAGE_LIVEDISPLAY_V2_0_READINGENHANCEMENT_H
