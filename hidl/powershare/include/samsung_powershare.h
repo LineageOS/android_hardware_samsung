3ddaa79a9511c1e991c4b1bfde9a03971fce1ba4
Bruno Martins <bgcngm@gmail.com>
2022-01-13 20:44:10 +0000

    hidl: powershare: Use pragma once
    Change-Id: I9cfeed5e79c35139f7532c522dad0ca25ed35fe1

--- hidl/powershare/include/samsung_powershare.h
+++ hidl/powershare/include/samsung_powershare.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The LineageOS Project
+ * Copyright (C) 2020-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,7 @@
  * limitations under the License.
  */
 
-#ifndef SAMSUNG_POWERSHARE_H
-#define SAMSUNG_POWERSHARE_H
+#pragma once
 
 #define POWERSHARE_PATH "/sys/class/power_supply/battery/wc_tx_en"
 #define POWERSHARE_STOP_CAPACITY_PATH "/sys/class/power_supply/battery/wc_tx_stop_capacity"
-
-#endif // SAMSUNG_POWERSHARE_H
