e858312178d5f73e37f0355f0a9a92fcca422eb4
Tim Zimmermann <tim@linux4.de>
2022-09-05 20:58:25 +0200

    SPenActions: Allow the user to reset S Pen MAC address
    * Like on stock
    
    Change-Id: I9df7b76214b22b57d89b523ef256f21a9874cc0c

--- SPenActions/src/org/lineageos/spenactions/settings/SettingsFragment.java
+++ SPenActions/src/org/lineageos/spenactions/settings/SettingsFragment.java
@@ -10,23 +10,37 @@ import android.bluetooth.BluetoothManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.view.View;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreferenceCompat;
 
+import com.android.settingslib.widget.ActionButtonsPreference;
+
+import org.lineageos.spenactions.BluetoothUtils;
 import org.lineageos.spenactions.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat
-        implements Preference.OnPreferenceChangeListener {
+        implements Preference.OnPreferenceChangeListener, View.OnClickListener {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.spen_settings, rootKey);
 
+        ActionButtonsPreference actionButtonsPreference =
+                findPreference(SettingsUtils.ACTION_BUTTONS);
+        actionButtonsPreference.setButton1Text(R.string.spen_reset);
+        actionButtonsPreference.setButton1OnClickListener(this);
+
         SwitchPreferenceCompat enableBluetoothPreference =
                 findPreference(SettingsUtils.SPEN_BLUETOOTH_ENABLE);
         enableBluetoothPreference.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        BluetoothUtils.resetSPenMAC(getActivity());
     }
 
     @Override
@@ -36,12 +50,7 @@ public class SettingsFragment extends PreferenceFragmentCompat
                     getActivity().getSystemService(BluetoothManager.class).getAdapter();
             if (btAdapter.getState() == BluetoothAdapter.STATE_ON) {
                 // Simulate bluetooth switch off/on to apply the new settings
-                Intent btState = new Intent(BluetoothAdapter.ACTION_STATE_CHANGED);
-                btState.setPackage(getActivity().getPackageName());
-                btState.putExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.STATE_OFF);
-                getActivity().sendBroadcastAsUser(btState, UserHandle.CURRENT);
-                btState.putExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.STATE_ON);
-                getActivity().sendBroadcastAsUser(btState, UserHandle.CURRENT);
+                BluetoothUtils.simulateReconnection(getActivity());
             }
         }
 
