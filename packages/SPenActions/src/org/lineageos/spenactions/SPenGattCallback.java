8ac094f3d6f3c52ae942de67de312328093d8cd0
Tim Zimmermann <tim@linux4.de>
2022-09-05 17:06:00 +0200

    SPenActions: Support updating S Pen MAC address
    * This doesn't change the pen's actual MAC address,
      it just changes the address we try to connect to
    
    Change-Id: Ib731c328ab52cc14a704de1885f29c1ff50efdff

--- packages/SPenActions/src/org/lineageos/spenactions/SPenGattCallback.java
+++ packages/SPenActions/src/org/lineageos/spenactions/SPenGattCallback.java
@@ -67,6 +67,7 @@ public class SPenGattCallback extends BluetoothGattCallback {
 
         if (newState == BluetoothProfile.STATE_CONNECTED) {
             Log.i(LOG_TAG, "SPen connected!");
+            mConnectionManager.onConnected();
             gatt.discoverServices();
         } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
             gatt.disconnect();
