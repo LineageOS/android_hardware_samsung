db76885a75fe0c6a3318c2e8fa59069dd32e9f1c
Tim Zimmermann <tim@linux4.de>
2022-09-05 20:58:25 +0200

    SPenActions: Allow the user to reset S Pen MAC address
    * Like on stock
    
    Change-Id: I9df7b76214b22b57d89b523ef256f21a9874cc0c

--- SPenActions/src/org/lineageos/spenactions/settings/SettingsFragment.java
+++ SPenActions/src/org/lineageos/spenactions/settings/SettingsFragment.java
@@ -10,23 +10,37 @@ import android.bluetooth.BluetoothManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.view.View;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreferenceCompat;
 
+import com.android.settingslib.widget.ActionButtonsPreference;
+
+import org.lineageos.spenactions.BluetoothUtils;
 import org.lineageos.spenactions.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat
-        implements Preference.OnPreferenceChangeListener {
+        implements Preference.OnPreferenceChangeListener, View.OnClickListener {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.spen_settings, rootKey);
 
+        ActionButtonsPreference actionButtonsPreference =
+                findPreference(SettingsUtils.ACTION_BUTTONS);
+        actionButtonsPreference.setButton1Text(R.string.spen_reset);
+        actionButtonsPreference.setButton1OnClickListener(this);
+
         SwitchPreferenceCompat enableBluetoothPreference =
                 findPreference(SettingsUtils.SPEN_BLUETOOTH_ENABLE);
         enableBluetoothPreference.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        BluetoothUtils.resetSPenMAC(getActivity());
     }
 
     @Override
