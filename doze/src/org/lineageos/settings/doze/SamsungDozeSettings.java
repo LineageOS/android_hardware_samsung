0c4a57235fd868f1daed8d9138156849670f4e21
Michael Bestas <mkbestas@gmail.com>
2024-03-08 20:15:39 +0200

    doze: Convert to SwitchPreferenceCompat
    Change-Id: Ic1cbaba37d499da1855af9c3930f2df426e2d3af
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- doze/src/org/lineageos/settings/doze/SamsungDozeSettings.java
+++ doze/src/org/lineageos/settings/doze/SamsungDozeSettings.java
@@ -25,7 +25,7 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.settingslib.widget.MainSwitchPreference;
 
@@ -36,11 +36,11 @@ public class SamsungDozeSettings extends PreferenceFragmentCompat
 
     private MainSwitchPreference mSwitchBar;
 
-    private SwitchPreference mAlwaysOnDisplayPreference;
-    private SwitchPreference mHandwavePreference;
-    private SwitchPreference mPickUpPreference;
-    private SwitchPreference mPocketPreference;
-    private SwitchPreference mWakeOnGesturePreference;
+    private SwitchPreferenceCompat mAlwaysOnDisplayPreference;
+    private SwitchPreferenceCompat mHandwavePreference;
+    private SwitchPreferenceCompat mPickUpPreference;
+    private SwitchPreferenceCompat mPocketPreference;
+    private SwitchPreferenceCompat mWakeOnGesturePreference;
 
     private Handler mHandler = new Handler();
 
@@ -71,7 +71,7 @@ public class SamsungDozeSettings extends PreferenceFragmentCompat
         mPocketPreference.setEnabled(dozeEnabled);
         mPocketPreference.setOnPreferenceChangeListener(this);
 
-        mWakeOnGesturePreference = (SwitchPreference) findPreference(Utils.WAKE_ON_GESTURE_KEY);
+        mWakeOnGesturePreference = findPreference(Utils.WAKE_ON_GESTURE_KEY);
         mWakeOnGesturePreference.setEnabled(dozeEnabled);
         mWakeOnGesturePreference.setOnPreferenceChangeListener(this);
 
