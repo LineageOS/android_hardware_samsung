2949576a29943df9cb4a5e56b95ff5df107d79df
Ruchit <risen@pixelexperience.org>
2024-03-12 21:13:48 +0530

    doze: mk->bp
    Fixes
    
    FAILED: /home/risen/android/builds/pe/a52q/target/product/a52q/obj/APPS/SamsungDoze_intermediates/dexpreopt.sh
    /bin/bash -c "/home/risen/android/builds/pe/a52q/host/linux-x86/bin/dexpreopt_gen -global_soong /home/risen/android/builds/pe/a52q/soong/dexpreopt_soong.config -global /home/risen/android/builds/pe/a52q/soong/dexpreopt_arm64/dexpreopt.config -module /home/risen/android/builds/pe/a52q/target/product/a52q/obj/APPS/SamsungDoze_intermediates/dexpreopt.config -dexpreopt_script /home/risen/android/builds/pe/a52q/target/product/a52q/obj/APPS/SamsungDoze_intermediates/dexpreopt.sh -out_dir /home/risen/android/builds/pe/a52q -product_packages /home/risen/android/builds/pe/a52q/target/product/a52q/obj/APPS/SamsungDoze_intermediates/product_packages.txt"
    error: Path is outside directory: /home/risen/android/builds/pe/a52q/target/product/a52q/obj/APPS/SamsungDoze_intermediates/product_packages.txt
    
    Change-Id: I503a688658a2879fc72180f370717f2d835c1efe

--- Android.mk
+++ Android.mk
@@ -37,7 +37,6 @@ endif
 
 ifeq ($(BOARD_VENDOR),samsung)
 include $(SAM_ROOT)/audio/Android.mk
-include $(SAM_ROOT)/doze/Android.mk
 include $(SAM_ROOT)/hidl/Android.mk
 include $(SAM_ROOT)/modemloader/Android.mk
 include $(SAM_ROOT)/ril/Android.mk
