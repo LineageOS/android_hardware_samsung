e2289dea3e4658a74e4ffe2e187e6d3a986a413a
Atakan <ata.kaner03@gmail.com>
2025-06-22 01:49:37 +0300

    aidl: fingerprint: Introduce SecUdfpsHelper
    SecUdfpsHelper is a fingerprint HAL addition that helps with UDFPS
    related operations on Samsung devices. It currently has two different
    purposes:
    
    * Providing a control over touchscreen.
    * Providing a way of monitoring HBM state on Qualcomm-based Samsung
    devices, with the help of newly introduced DRM property.
    
    1- Touchscreen control
    -------------------------------------------------------------------
    Touchscreen control offers two different ways to do this:
    
    * Default method: using set/get functions to write commands to the
    touchscreen cmd node.
    * SysInput method: using Samsung SysInput service.
    
    Default method provides a more legacy way of handling, while SysInput
    method can be useful for foldable devices, i.e. devices with multiple
    input hardwares. The SysInput HAL provides communication with each
    input hardware (Multi-TSP, Keyboard etc.) so that we don't have to
    define each path individually.
    
    Note that the SysInput method is completely optional and maintainers
    are not required to provide the HAL for their devices.
    
    2- HBM monitoring (Qualcomm only)
    ---------------------------------------------------------------------
    HBM monitoring requires the newly introduced DRM feature and 2 new
    sysfs interfaces to work properly.
    
    HBM monitoring will register an inotify instance to the HBM status path
    and start listening. When the status path is changed, it will read the
    file and if HBM is enabled, it will write the HBM brightness value to
    mask_brightness sysfs.
    
    Since each device needs a specific brightness value due to different
    screens, maintainers can expose the required brightness value via sysfs.
    Fingerprint HAL will read and use this value to write the HBM
    brightness.
    
    If sysfs is not exposed, fingerprint HAL will use the default value of
    319.
    
    Note that this monitoring logic was written with framework dimming in
    mind. Kernel dimming implementations may not work or may require an
    adaptation.
    
    Change-Id: I17b54744d7ce2cdcd40b175bafec06eb48894db5

---
+++ aidl/fingerprint/SemInputConstants.h
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace biometrics {
+namespace fingerprint {
+
+enum class SemInputDevice : int {
+    CURRENT_TSP = 0,
+    DEFAULT_TSP = 1,
+    EXTRA_TSP = 2,
+    SPEN = 11,
+    KEY = 21,
+    KEYBOARD = 31,
+    TAAS = 41,
+    UNSPECIFIED = 100,
+};
+
+enum class SemInputCommand : int {
+    NONE = 0,
+    GAME = 1,
+    SCAN_RATE = 2,
+    REFRESH_RATE = 3,
+    GLOVE = 4,
+    CLEAR_COVER = 5,
+    ORIENTATION = 6,
+    PROX_LP_SCAN = 7,
+    GRIP_DATA = 8,
+    SIP = 9,
+    NOTE_APP = 10,
+    TEMPERATURE = 11,
+    SPAY = 12,
+    STYLUS = 13,
+    BRUSH = 14,
+    AOD_RECT = 15,
+    AOD = 16,
+    FOD = 17,
+    FOD_ICON_VISIBLE = 18,
+    FOD_RECT = 19,
+    FOD_LP = 20,
+    SINGLETAP = 21,
+    EAR_DETECT = 22,
+    EXTERNAL_NOISE = 23,
+    TOUCHABLE_AREA = 24,
+    FP_INT_CONTROL = 25,
+    SYNC_CHANGED = 26,
+    POCKET_MODE = 27,
+    LOW_SENSITIVITY = 28,
+    CHARGER = 29,
+    AOT = 30,
+    FOLD_STATE = 31,
+    WIRELESS_CHARGER = 32,
+    TWO_FINGER_DOUBLETAP = 33,
+    SPEN_COVER_TYPE = 34,
+    SPEN_SAVING_MODE = 35,
+    SPEN_POWER = 36,
+    SPEN_BLE_CHARGING = 37,
+    SPEN_SCREEN_OFF_MEMO = 38,
+    SPEN_PDCT_LOWSENSITIVITY = 39,
+    SPEN_LOWCURRENT = 40,
+    ALWAYS_LOW_POWER_MODE = 41,
+    BEZEL = 42,
+    BODY_DETECTION = 43,
+    AWD = 44,
+    NFC_FIELD = 45,
+    SPEN_SET_WIRELESS_CHARGER_TX_ID = 46,
+    AOD_NOTI_RECT = 47,
+    FAST_RESPONSE = 48,
+};
+
+enum class SemInputProperty : int {
+    NONE = 0,
+    FEATURE = 1,
+    CMD_LIST = 2,
+    SCRUB_POS = 3,
+    FOD_INFO = 4,
+    FOD_POS = 5,
+    AOD_ACTIVE_AREA = 6,
+    AOD_ENABLE = 7,
+    EPEN_POS = 8,
+    PROX_OFF = 9,
+    HW_PARAM = 10,
+    LP_DUMP = 11,
+    BLE_CHARGING = 12,
+    EPEN_SAVING = 13,
+    EPEN_MEMO = 14,
+    HAND_EDGE = 15,
+    EPEN_WCHARGING = 16,
+    ENABLED = 17,
+    CMD = 18,
+};
+
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace hardware
+}  // namespace android
+}  // namespace aidl
