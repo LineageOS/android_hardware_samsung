7a083424425d1d751090b8d9b1c10e21467b2152
Atakan <ata.kaner03@gmail.com>
2025-06-22 01:49:37 +0300

    aidl: fingerprint: Introduce SecUdfpsHelper
    SecUdfpsHelper is a fingerprint HAL addition that helps with UDFPS
    related operations on Samsung devices. It currently has two different
    purposes:
    
    * Providing a control over touchscreen.
    * Providing a way of monitoring HBM state on Qualcomm-based Samsung
    devices, with the help of newly introduced DRM property.
    
    1- Touchscreen control
    -------------------------------------------------------------------
    Touchscreen control offers two different ways to do this:
    
    * Default method: using set/get functions to write commands to the
    touchscreen cmd node.
    * SysInput method: using Samsung SysInput service.
    
    Default method provides a more legacy way of handling, while SysInput
    method can be useful for foldable devices, i.e. devices with multiple
    input hardwares. The SysInput HAL provides communication with each
    input hardware (Multi-TSP, Keyboard etc.) so that we don't have to
    define each path individually.
    
    Note that the SysInput method is completely optional and maintainers
    are not required to provide the HAL for their devices.
    
    2- HBM monitoring (Qualcomm only)
    ---------------------------------------------------------------------
    HBM monitoring requires the newly introduced DRM feature and 2 new
    sysfs interfaces to work properly.
    
    HBM monitoring will register an inotify instance to the HBM status path
    and start listening. When the status path is changed, it will read the
    file and if HBM is enabled, it will write the HBM brightness value to
    mask_brightness sysfs.
    
    Since each device needs a specific brightness value due to different
    screens, maintainers can expose the required brightness value via sysfs.
    Fingerprint HAL will read and use this value to write the HBM
    brightness.
    
    If sysfs is not exposed, fingerprint HAL will use the default value of
    319.
    
    Note that this monitoring logic was written with framework dimming in
    mind. Kernel dimming implementations may not work or may require an
    adaptation.
    
    Change-Id: I17b54744d7ce2cdcd40b175bafec06eb48894db5

---
+++ aidl/fingerprint/SecUdfpsHelper.h
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/samsung/hardware/sysinput/ISehSysInputDev.h>
+#include <aidl/vendor/samsung/hardware/sysinput/SehIntStringParcel.h>
+#include <android-base/logging.h>
+#include <android/binder_interface_utils.h>
+#include <android/binder_manager.h>
+#include <memory>
+
+#include "SemInputConstants.h"
+
+#define UDFPS_TAG "[SecUdfpsHelper]: "
+
+inline std::shared_ptr<aidl::vendor::samsung::hardware::sysinput::ISehSysInputDev>
+getSehSysInputDev(const std::string& instanceName = "default") {
+    using aidl::vendor::samsung::hardware::sysinput::ISehSysInputDev;
+
+#if USE_SYSINPUT_HAL
+    const std::string sehInstance =
+            std::string() + ISehSysInputDev::descriptor + "/" + instanceName;
+
+    ndk::SpAIBinder binder(AServiceManager_waitForService(sehInstance.c_str()));
+    if (!binder.get()) {
+        LOG(ERROR) << "Failed to get ISehSysInputDev service: " << sehInstance;
+        return nullptr;
+    }
+
+    std::shared_ptr<ISehSysInputDev> dev = ISehSysInputDev::fromBinder(binder);
+    if (!dev) {
+        LOG(ERROR) << "Failed to register ISehSysInputDev interface";
+    }
+
+    return dev;
+#else
+    LOG(ERROR) << "Fingerprint HAL doesn't use SysInput Interface";
+    return nullptr;
+#endif
+}
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace biometrics {
+namespace fingerprint {
+
+using ::aidl::vendor::samsung::hardware::sysinput::ISehSysInputDev;
+
+class SecUdfpsHelper {
+  public:
+    explicit SecUdfpsHelper(std::shared_ptr<ISehSysInputDev> sehInput);
+
+#if USE_SYSINPUT_HAL
+    void runCmd(const std::string& cmdString);
+    int setFodEnable(int i, int i2 = 0, int i3 = 0);
+    int setFodIconVisible(int i);
+    int setFodLpMode(int i);
+    int setFodRect(const std::string& fodRect);
+#else
+    void setFodEnable(bool enable);
+    void setFodRect(const std::string& fodRect);
+#endif
+
+    template <typename T>
+    T get(const std::string& path, const T& def);
+    template <typename T>
+    void set(const std::string& path, const T& value);
+
+  private:
+    std::shared_ptr<ISehSysInputDev> mSehInput;
+
+#if USE_SYSINPUT_HAL
+    void runCommand(int inputDevice, const std::string& cmdString);
+    int setProperty(int inputDevice, int inputCommand, const std::string& cmdString);
+    std::string getProperty(int inputDevice, int inputCommand);
+#else
+    static constexpr const char* TSP_CMD = "/sys/class/sec/tsp/cmd";
+    static constexpr const char* TSP_CMD_LIST = "/sys/class/sec/tsp/cmd_list";
+#endif
+
+#ifdef SUPPORTS_QCOM_HBM
+    class HbmWatcher {
+      public:
+        void start();
+
+      private:
+        void monitorLoop();
+    };
+    HbmWatcher mHbmWatcher;
+
+    /*
+     * finger_mask_state -> Provides info about HBM status through DRM property
+     * finger_mask_brightness -> Provides default brightness value used by HBM to FP HAL
+     * actual_mask_brightness -> Current brightness value of the HBM
+     * mask_brightness -> Sets brightness of the HBM
+     * To-Do: fp_green_circle support?
+     */
+    static constexpr const char* FP_MASK_ENABLED =
+            "/sys/devices/virtual/lcd/panel/finger_mask_state";
+    static constexpr const char* FP_MASK_DEFAULT_BRIGHT =
+            "/sys/devices/virtual/lcd/panel/finger_mask_brightness";
+    static constexpr const char* HBM_BRIGHT_RO =
+            "/sys/devices/virtual/lcd/panel/actual_mask_brightness";
+    static constexpr const char* HBM_BRIGHT_WO = "/sys/devices/virtual/lcd/panel/mask_brightness";
+    static constexpr int DEFAULT_MASK_BRIGHTNESS = 319;
+#endif
+};
+
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace hardware
+}  // namespace android
+}  // namespace aidl
