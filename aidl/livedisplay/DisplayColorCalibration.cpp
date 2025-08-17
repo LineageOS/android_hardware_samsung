c054ceecbfe04ac4c81ecb98c646541b0fda9afa
Bruno Martins <bgcngm@gmail.com>
2025-08-17 12:41:38 +0100

    livedisplay: Migrate to AIDL
    Change-Id: I4a67910fdc09a3350c5ee7215d5ac9b69c977af0

---
+++ aidl/livedisplay/DisplayColorCalibration.cpp
@@ -0,0 +1,77 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/file.h>
+#include <android-base/strings.h>
+#include <livedisplay/samsung/DisplayColorCalibration.h>
+
+#include <fstream>
+
+using android::base::ReadFileToString;
+using android::base::Split;
+using android::base::Trim;
+using android::base::WriteStringToFile;
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace samsung {
+
+static constexpr const char* kRGBPath = "/sys/class/mdnie/mdnie/sensorRGB";
+
+bool DisplayColorCalibration::isSupported() {
+    std::fstream file(kRGBPath, file.in | file.out);
+    return file.good();
+}
+
+// Methods from ::aidl::vendor::lineage::livedisplay::BnDisplayColorCalibration follow.
+ndk::ScopedAStatus DisplayColorCalibration::getMaxValue(int32_t* _aidl_return) {
+    *_aidl_return = 255;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus DisplayColorCalibration::getMinValue(int32_t* _aidl_return) {
+    *_aidl_return = 1;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus DisplayColorCalibration::getCalibration(
+        std::vector<int32_t>* _aidl_return) {
+    std::vector<int32_t> rgb;
+    std::string tmp;
+
+    if (ReadFileToString(kRGBPath, &tmp)) {
+        std::vector<std::string> colors = Split(Trim(tmp), " ");
+        for (const std::string& color : colors) {
+            rgb.push_back(std::stoi(color));
+        }
+    } else {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    *_aidl_return = rgb;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus DisplayColorCalibration::setCalibration(const std::vector<int32_t>& rgb) {
+    std::string contents;
+
+    for (const int32_t& color : rgb) {
+        contents += std::to_string(color) + " ";
+    }
+
+    if (!WriteStringToFile(Trim(contents), kRGBPath, true)) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    return ndk::ScopedAStatus::ok();
+}
+
+}  // namespace samsung
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
--- aidl/livedisplay/service.cpp
+++ aidl/livedisplay/service.cpp
@@ -1,81 +1,66 @@
 /*
- * Copyright (C) 2019-2025 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-FileCopyrightText: 2019-2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_TAG "vendor.lineage.livedisplay@2.0-service.samsung-qcom"
+#define LOG_TAG "vendor.lineage.livedisplay-service.samsung-qcom"
 
 #include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
 #include <binder/ProcessState.h>
-#include <hidl/HidlTransportSupport.h>
 #include <livedisplay/samsung/AdaptiveBacklight.h>
 #include <livedisplay/samsung/DisplayColorCalibration.h>
 #include <livedisplay/samsung/DisplayModes.h>
 #include <livedisplay/samsung/SunlightEnhancement.h>
 
-using android::OK;
-using android::sp;
-using android::status_t;
-using android::hardware::configureRpcThreadpool;
-using android::hardware::joinRpcThreadpool;
-
-using vendor::lineage::livedisplay::V2_0::samsung::AdaptiveBacklight;
-using vendor::lineage::livedisplay::V2_0::samsung::DisplayColorCalibration;
-using vendor::lineage::livedisplay::V2_0::samsung::DisplayModes;
-using vendor::lineage::livedisplay::V2_0::samsung::SunlightEnhancement;
+using ::aidl::vendor::lineage::livedisplay::samsung::AdaptiveBacklight;
+using ::aidl::vendor::lineage::livedisplay::samsung::DisplayColorCalibration;
+using ::aidl::vendor::lineage::livedisplay::samsung::DisplayModes;
+using ::aidl::vendor::lineage::livedisplay::samsung::SunlightEnhancement;
 
 int main() {
-    sp<AdaptiveBacklight> adaptiveBacklight;
-    sp<DisplayColorCalibration> displayColorCalibration;
-    sp<DisplayModes> displayModes;
-    sp<SunlightEnhancement> sunlightEnhancement;
-    status_t status;
+    android::ProcessState::self()->setThreadPoolMaxThreadCount(1);
+    android::ProcessState::self()->startThreadPool();
+
+    std::shared_ptr<AdaptiveBacklight> adaptiveBacklight =
+            ndk::SharedRefBase::make<AdaptiveBacklight>();
+    std::shared_ptr<DisplayColorCalibration> displayColorCalibration =
+            ndk::SharedRefBase::make<DisplayColorCalibration>();
+    std::shared_ptr<DisplayModes> displayModes = ndk::SharedRefBase::make<DisplayModes>();
+    std::shared_ptr<SunlightEnhancement> sunlightEnhancement =
+            ndk::SharedRefBase::make<SunlightEnhancement>();
+    binder_status_t status;
 
     LOG(INFO) << "LiveDisplay HAL service is starting.";
 
-    adaptiveBacklight = new AdaptiveBacklight();
     if (adaptiveBacklight == nullptr) {
         LOG(ERROR) << "Can not create an instance of LiveDisplay HAL AdaptiveBacklight Iface, "
                       "exiting.";
         goto shutdown;
     }
 
-    displayColorCalibration = new DisplayColorCalibration();
     if (displayColorCalibration == nullptr) {
         LOG(ERROR) << "Can not create an instance of LiveDisplay HAL DisplayColorCalibration "
                       "Iface, exiting.";
         goto shutdown;
     }
 
-    displayModes = new DisplayModes();
     if (displayModes == nullptr) {
         LOG(ERROR) << "Can not create an instance of LiveDisplay HAL DisplayModes Iface, exiting.";
         goto shutdown;
     }
 
-    sunlightEnhancement = new SunlightEnhancement();
     if (sunlightEnhancement == nullptr) {
         LOG(ERROR) << "Can not create an instance of LiveDisplay HAL SunlightEnhancement Iface, "
                       "exiting.";
         goto shutdown;
     }
 
-    configureRpcThreadpool(1, true /*callerWillJoin*/);
-
     if (adaptiveBacklight->isSupported()) {
-        status = adaptiveBacklight->registerAsService();
-        if (status != OK) {
+        std::string instance = std::string(AdaptiveBacklight::descriptor) + "/default";
+        status = AServiceManager_addService(adaptiveBacklight->asBinder().get(), instance.c_str());
+        if (status != STATUS_OK) {
             LOG(ERROR) << "Could not register service for LiveDisplay HAL AdaptiveBacklight Iface ("
                        << status << ")";
             goto shutdown;
@@ -83,8 +68,10 @@ int main() {
     }
 
     if (displayColorCalibration->isSupported()) {
-        status = displayColorCalibration->registerAsService();
-        if (status != OK) {
+        std::string instance = std::string(DisplayColorCalibration::descriptor) + "/default";
+        status = AServiceManager_addService(displayColorCalibration->asBinder().get(),
+                                            instance.c_str());
+        if (status != STATUS_OK) {
             LOG(ERROR) << "Could not register service for LiveDisplay HAL DisplayColorCalibration "
                           "Iface ("
                        << status << ")";
@@ -93,8 +80,9 @@ int main() {
     }
 
     if (displayModes->isSupported()) {
-        status = displayModes->registerAsService();
-        if (status != OK) {
+        std::string instance = std::string(DisplayModes::descriptor) + "/default";
+        status = AServiceManager_addService(displayModes->asBinder().get(), instance.c_str());
+        if (status != STATUS_OK) {
             LOG(ERROR) << "Could not register service for LiveDisplay HAL DisplayModes Iface ("
                        << status << ")";
             goto shutdown;
@@ -102,8 +90,10 @@ int main() {
     }
 
     if (sunlightEnhancement->isSupported()) {
-        status = sunlightEnhancement->registerAsService();
-        if (status != OK) {
+        std::string instance = std::string(SunlightEnhancement::descriptor) + "/default";
+        status =
+                AServiceManager_addService(sunlightEnhancement->asBinder().get(), instance.c_str());
+        if (status != STATUS_OK) {
             LOG(ERROR)
                     << "Could not register service for LiveDisplay HAL SunlightEnhancement Iface ("
                     << status << ")";
@@ -112,11 +102,10 @@ int main() {
     }
 
     LOG(INFO) << "LiveDisplay HAL service is ready.";
-    joinRpcThreadpool();
-    // Should not pass this line
+    ABinderProcess_joinThreadPool();
 
 shutdown:
     // In normal operation, we don't expect the thread pool to shutdown
     LOG(ERROR) << "LiveDisplay HAL service is shutting down.";
-    return 1;
+    return EXIT_FAILURE;
 }
