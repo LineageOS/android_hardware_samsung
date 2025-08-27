745825591fc2bf377f9e0b73ab6e63a3dcbc6344
ExtremeXT <extremextdev@gmail.com>
2025-08-27 22:55:28 +0300

    touch: Add Single Tap gesture support
    Co-authored-by: Simon1511 <simon2002.schoenmackers@gmail.com>
    Change-Id: I0400802734471bffcf90d9711dbb7771fc7c8c86
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- aidl/touch/TouchscreenGesture.h
+++ aidl/touch/TouchscreenGesture.h
@@ -6,8 +6,9 @@
 #pragma once
 
 #include <aidl/vendor/lineage/touch/BnTouchscreenGesture.h>
-#include <map>
 #include <samsung_touch.h>
+#include <fstream>
+#include <map>
 
 namespace aidl {
 namespace vendor {
@@ -27,6 +28,10 @@ class TouchscreenGesture : public BnTouchscreenGesture {
         const char* name;
     } GestureInfo;
     static const std::map<int32_t, GestureInfo> kGestureInfoMap;  // id -> info
+    static const std::pair<int32_t, GestureInfo> kSingleTapEntry;
+
+    bool mHasEpenGestureNode = std::ifstream(EPEN_GESTURE_NODE).good();
+    bool mHasTspCmdNode = std::ifstream(TSP_CMD_NODE).good();
 };
 
 }  // namespace touch
