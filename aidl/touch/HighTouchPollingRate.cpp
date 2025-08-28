828ae7d035aa50f34713b2d5be1a593f62ae4b30
ExtremeXT <extremextdev@gmail.com>
2025-08-28 18:59:56 +0300

    touch: Also support set_scan_rate TSP CMD
    This adds support for set_scan_rate SEC TSP command,
    which is used by devices like the S20-S22 Series (non-FE).
    
    Change-Id: I643832df015220874d728b1ef54533c07e59f885
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- aidl/touch/HighTouchPollingRate.cpp
+++ aidl/touch/HighTouchPollingRate.cpp
@@ -13,15 +13,7 @@ namespace lineage {
 namespace touch {
 
 bool HighTouchPollingRate::isSupported() {
-    std::ifstream file(TSP_CMD_LIST_NODE);
-    if (file.is_open()) {
-        std::string line;
-        while (getline(file, line)) {
-            if (!line.compare("set_game_mode")) return true;
-        }
-        file.close();
-    }
-    return false;
+    return !mHtprCmd.empty();
 }
 
 ndk::ScopedAStatus HighTouchPollingRate::getEnabled(bool* _aidl_return) {
@@ -29,7 +21,7 @@ ndk::ScopedAStatus HighTouchPollingRate::getEnabled(bool* _aidl_return) {
     if (file.is_open()) {
         std::string line;
         getline(file, line);
-        *_aidl_return = !line.compare("set_game_mode,1:OK");
+        *_aidl_return = !line.compare(mHtprCmd + ",1:OK");
         file.close();
     }
 
@@ -38,7 +30,7 @@ ndk::ScopedAStatus HighTouchPollingRate::getEnabled(bool* _aidl_return) {
 
 ndk::ScopedAStatus HighTouchPollingRate::setEnabled(bool enabled) {
     std::ofstream file(TSP_CMD_NODE);
-    file << "set_game_mode," << (enabled ? "1" : "0");
+    file << (mHtprCmd + ",") << (enabled ? "1" : "0");
 
     return ndk::ScopedAStatus::ok();
 }
