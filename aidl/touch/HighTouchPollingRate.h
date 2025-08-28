757b32f807207dcf6a0ceeec1daeabc19d8d29b0
ExtremeXT <extremextdev@gmail.com>
2025-08-28 18:59:56 +0300

    touch: Add support for set_scan_rate TSP CMD
    Change-Id: I643832df015220874d728b1ef54533c07e59f885
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- aidl/touch/HighTouchPollingRate.h
+++ aidl/touch/HighTouchPollingRate.h
@@ -7,6 +7,7 @@
 
 #include <aidl/vendor/lineage/touch/BnHighTouchPollingRate.h>
 #include <samsung_touch.h>
+#include <fstream>
 
 namespace aidl {
 namespace vendor {
@@ -15,10 +16,28 @@ namespace touch {
 
 class HighTouchPollingRate : public BnHighTouchPollingRate {
   public:
+    HighTouchPollingRate() {
+        std::ifstream file(TSP_CMD_LIST_NODE);
+        if (file.is_open()) {
+            mHtprCmd = "";
+            std::string line;
+            while (getline(file, line)) {
+                if (!line.compare("set_game_mode") || !line.compare("set_scan_rate")) {
+                    mHtprCmd = line;
+                    break;
+                }
+            }
+            file.close();
+        }
+    }
+
     bool isSupported();
 
     ndk::ScopedAStatus getEnabled(bool* _aidl_return) override;
     ndk::ScopedAStatus setEnabled(bool enabled) override;
+
+  private:
+    std::string mHtprCmd;
 };
 
 }  // namespace touch
