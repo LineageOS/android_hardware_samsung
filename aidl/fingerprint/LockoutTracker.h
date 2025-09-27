9206b409c8a8cfa84eeee4bb06d1835b92e2628a
Bruno Martins <bgcngm@gmail.com>
2025-09-27 13:11:32 +0100

    aidl: fingerprint: Add missing include
    Spotted after clang-format formatting:
    
      hardware/samsung/aidl/fingerprint/LockoutTracker.h:26:5: error: unknown type name 'int64_t'
         26 |     int64_t getLockoutTimeLeft();
            |     ^
      hardware/samsung/aidl/fingerprint/LockoutTracker.h:29:5: error: unknown type name 'int32_t'
         29 |     int32_t mFailedCount = 0;
            |     ^
      hardware/samsung/aidl/fingerprint/LockoutTracker.h:30:5: error: unknown type name 'int64_t'
         30 |     int64_t mLockoutTimedStart;
            |     ^
    
    Change-Id: I2cc02e0bdda9e3302aac8bffc5c6022caee96b34

--- aidl/fingerprint/LockoutTracker.h
+++ aidl/fingerprint/LockoutTracker.h
@@ -1,10 +1,12 @@
 /*
- * Copyright (C) 2024 The LineageOS Project
+ * Copyright (C) 2024-2025 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #pragma once
+
+#include <cstdint>
 
 namespace aidl {
 namespace android {
