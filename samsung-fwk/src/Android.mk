4eb9893bef526b73f5668980d1d9e34f9a3295e9
Atakan <ata.kaner03@gmail.com>
2025-11-02 17:25:31 +0300

    [DNM] samsung: Introduce samsung-fwk
    A set of Samsung API's used heavily in their framework and apps.
    
    Change-Id: I8de9e40c316c19cec1fc63eabe847a8258c849ed

---
+++ samsung-fwk/src/Android.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += samsung-fwk
+PRODUCT_BOOT_JARS += samsung-fwk
---
+++ samsung-fwk/src/com/samsung/android/feature/IFloatingFeature.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.samsung.android.feature;
+
+public interface IFloatingFeature {
+    boolean getBoolean(String str);
+    int getInt(String str);
+    String getString(String str);
+}
