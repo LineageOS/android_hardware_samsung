71690000bbd0bdc30c7a0d1f7731d1630316d41f
borbelyvince <borbely.vince.11@gmail.com>
2025-11-15 11:46:32 +0100

    aidl: vibrator: Add support for composite effects
    OWT imported from https://github.com/CirrusLogic/cirrus-hal/tree/owt_lib,
    the first parameter of dspmem_chunk_create changed to uint8_t * to avoid
    void pointer arithmetic.
    
    Co-authored-by: ExtremeXT <extremext@lineageos.org>
    Change-Id: I19614b2f593991ed1912b22e701f5e653b45227b
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- aidl/vibrator/Vibrator.cpp
+++ aidl/vibrator/Vibrator.cpp
@@ -5,6 +5,9 @@
  */
 
 #include "Vibrator.h"
+extern "C" {
+#include "owt.h"
+}
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
@@ -37,6 +40,14 @@ static std::map<Effect, std::pair<short, int>> FF_EFFECT_IDS{{Effect::CLICK, {1,
                                                              {Effect::TICK, {41, 25}},
                                                              {Effect::HEAVY_CLICK, {14, 100}},
                                                              {Effect::TEXTURE_TICK, {41, 25}}};
+
+std::map<CompositePrimitive, std::pair<short, int>> FF_PRIMITIVE_IDS{
+        {CompositePrimitive::NOOP, {0, 0}},           {CompositePrimitive::CLICK, {1, 20}},
+        {CompositePrimitive::THUD, {124, 300}},       {CompositePrimitive::SPIN, {123, 130}},
+        {CompositePrimitive::QUICK_RISE, {121, 150}}, {CompositePrimitive::SLOW_RISE, {122, 500}},
+        {CompositePrimitive::QUICK_FALL, {120, 100}}, {CompositePrimitive::LIGHT_TICK, {41, 20}},
+        {CompositePrimitive::LOW_TICK, {119, 20}},
+};
 
 #ifdef VIBRATOR_SUPPORTS_DURATION_AMPLITUDE_CONTROL
 static std::map<EffectStrength, float> DURATION_AMPLITUDE = {
@@ -96,9 +107,13 @@ Vibrator::Vibrator() {
                     writeNode("/sys/class/sec_vib_inputff/control/use_sep_index", 1);
                     if (nodeExists(VIBRATOR_FUNCTIONS_PATH)) {
                         std::string contents;
-                        if (ReadFileToString(VIBRATOR_FUNCTIONS_PATH, &contents) &&
-                            contents.find("COMMON_INPUTFF_INTERFACE") != std::string::npos) {
-                            mUsesCommonFFInterface = true;
+                        if (ReadFileToString(VIBRATOR_FUNCTIONS_PATH, &contents)) {
+                            if (contents.find("COMMON_INPUTFF_INTERFACE") != std::string::npos) {
+                                mUsesCommonFFInterface = true;
+                            }
+                            if (contents.find("PRIMITIVE_EFFECT_COMPOSE") != std::string::npos) {
+                                mSupportsPrimitives = true;
+                            }
                         }
                     }
                     break;
@@ -127,6 +142,9 @@ ndk::ScopedAStatus Vibrator::getCapabilities(int32_t* _aidl_return) {
 
     if (mIsForceFeedbackVibrator) {
         *_aidl_return |= IVibrator::CAP_AMPLITUDE_CONTROL;
+        if (mSupportsPrimitives) {
+            *_aidl_return |= IVibrator::CAP_COMPOSE_EFFECTS;
+        }
     }
 
     return ndk::ScopedAStatus::ok();
@@ -290,156 +308,286 @@ ndk::ScopedAStatus Vibrator::setExternalControl(bool enabled) {
     return ndk::ScopedAStatus::ok();
 }
 
-ndk::ScopedAStatus Vibrator::getCompositionDelayMax(int32_t* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
+ndk::ScopedAStatus Vibrator::getCompositionDelayMax(int32_t* _aidl_return) {
+    if (!mSupportsPrimitives)
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
 
-ndk::ScopedAStatus Vibrator::getCompositionSizeMax(int32_t* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getSupportedPrimitives(
-        std::vector<CompositePrimitive>* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getPrimitiveDuration(CompositePrimitive /*primitive*/,
-                                                  int32_t* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::compose(const std::vector<CompositeEffect>& /*composite*/,
-                                     const std::shared_ptr<IVibratorCallback>& /*callback*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getSupportedAlwaysOnEffects(std::vector<Effect>* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::alwaysOnEnable(int32_t /*id*/, Effect /*effect*/,
-                                            EffectStrength /*strength*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::alwaysOnDisable(int32_t /*id*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getResonantFrequency(float* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getQFactor(float* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getFrequencyResolution(float* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getFrequencyMinimum(float* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getBandwidthAmplitudeMap(std::vector<float>* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getPwlePrimitiveDurationMax(int32_t* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getPwleCompositionSizeMax(int32_t* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::getSupportedBraking(std::vector<Braking>* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::composePwle(const std::vector<PrimitivePwle>& /*composite*/,
-                                         const std::shared_ptr<IVibratorCallback>& /*callback*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::activate(uint32_t timeoutMs) {
-    std::lock_guard<std::mutex> lock{mMutex};
-    if (mIsTimedOutVibrator) {
-        return writeNode(VIBRATOR_TIMEOUT_PATH, timeoutMs);
-    }
-    if (mIsForceFeedbackVibrator) {
-        struct input_event event{
-                .type = EV_FF,
-                .code = 0,
-                .value = timeoutMs != 0,
-        };
-        if (write(mVibratorFd, &event, sizeof(event)) == -1)
-            return ndk::ScopedAStatus::fromExceptionCode(STATUS_UNKNOWN_ERROR);
-        return ndk::ScopedAStatus::ok();
-    }
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-}
-
-ndk::ScopedAStatus Vibrator::uploadFFEffect(std::vector<int16_t> effectData, int timeoutMs) {
-    int ret;
-
-    // Remove previously uploaded effect in case it exists
-    ret = ioctl(mVibratorFd, EVIOCRMFF, 0);
-    if (ret == -1) {
-        LOG(WARNING) << "Failed to remove effect";
-    }
-
-    struct ff_effect effect = {
-            .id = -1,
-            .replay.length = static_cast<uint16_t>(timeoutMs),
-    };
-
-    if (mUsesCommonFFInterface && effectData[0] == 0) {
-        effect.type = FF_CONSTANT;
-    } else {
-        effect.type = FF_PERIODIC;
-        effect.u.periodic = {
-                .waveform = FF_CUSTOM,
-                .custom_len = static_cast<uint32_t>(effectData.size()),
-                .custom_data = effectData.data(),
-        };
-    }
-
-    ret = ioctl(mVibratorFd, EVIOCSFF, &effect);
-    if (ret == -1) {
-        LOG(ERROR) << "Effect upload failed: " << errno;
-        return ndk::ScopedAStatus::fromStatus(STATUS_UNKNOWN_ERROR);
-    }
+    *_aidl_return = (mUsesCommonFFInterface ? COMMON_MAX_COMPOSE_DELAY : WT_MAX_DELAY);
     return ndk::ScopedAStatus::ok();
 }
 
-float Vibrator::strengthToAmplitude(EffectStrength strength, ndk::ScopedAStatus* status) {
-    *status = ndk::ScopedAStatus::ok();
+ndk::ScopedAStatus Vibrator::getCompositionSizeMax(int32_t* _aidl_return) {
+    if (!mSupportsPrimitives)
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
 
-    switch (strength) {
-        case EffectStrength::LIGHT:
-            return AMPLITUDE_LIGHT;
-        case EffectStrength::MEDIUM:
-            return AMPLITUDE_MEDIUM;
-        case EffectStrength::STRONG:
-            return AMPLITUDE_STRONG;
+    *_aidl_return = (mUsesCommonFFInterface ? COMMON_MAX_COMPOSE_EFFECT / 2 : WT_MAX_SECTIONS / 2);
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus Vibrator::getSupportedPrimitives(std::vector<CompositePrimitive>* _aidl_return) {
+    if (!mSupportsPrimitives)
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+
+    for (auto primitive : FF_PRIMITIVE_IDS) _aidl_return->push_back(primitive.first);
+
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus Vibrator::getPrimitiveDuration(CompositePrimitive primitive,
+                                                  int32_t* _aidl_return) {
+    if (!mSupportsPrimitives)
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+
+    *_aidl_return = FF_PRIMITIVE_IDS.at(primitive).second;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus Vibrator::compose(const std::vector<CompositeEffect>& composite,
+                                     const std::shared_ptr<IVibratorCallback>& callback) {
+    if (!mSupportsPrimitives)
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    if (composite.empty() ||
+        (mUsesCommonFFInterface && composite.size() > COMMON_MAX_COMPOSE_EFFECT / 2) ||
+        (!mUsesCommonFFInterface && composite.size() > WT_MAX_SECTIONS / 2))
+        return ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_ARGUMENT);
+
+    int len, totalDuration = 0;
+    int16_t data[WT_TYPE12_PWLE_SINGLE_PACKED_MAX / 2];
+    std::string effect_str;
+    std::vector<int16_t> effectData;
+    struct common_inputff_effects input_compose = {};
+
+    for (auto segment : composite) {
+        if (segment.delayMs < 0 ||
+            (mUsesCommonFFInterface && segment.delayMs > COMMON_MAX_COMPOSE_DELAY) ||
+            (!mUsesCommonFFInterface && segment.delayMs > WT_MAX_DELAY)) {
+            return ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_ARGUMENT);
+            if (FF_PRIMITIVE_IDS.find(segment.primitive) == FF_PRIMITIVE_IDS.end() ||
+                segment.scale < 0 || segment.scale > 1)
+                return ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_ARGUMENT);
+
+            if (segment.primitive == CompositePrimitive::NOOP) {
+                // No haptic effect. Used to generate extended delays between primitives.
+                if (segment.delayMs > 0) {
+                    if (mUsesCommonFFInterface)
+                        input_compose.effects[input_compose.num_of_effects++] = {
+                                0, 0, 0, segment.delayMs, 0};
+                    else
+                        effect_str.append(std::format("{} ", segment.delayMs));
+                }
+                totalDuration += segment.delayMs;
+                continue;
+            }
+
+            // Consider 15% the lowest "feelable" amplitude
+            int scale = round(segment.scale * 85 + 15);
+
+            uint32_t id = FF_PRIMITIVE_IDS.at(segment.primitive).first;
+
+            int duration = 0;
+            getPrimitiveDuration(segment.primitive, &duration);
+            totalDuration += duration + segment.delayMs;
+
+            if (mUsesCommonFFInterface) {
+                if (segment.delayMs > 0) {
+                    input_compose.effects[input_compose.num_of_effects++] = {0, 0, 0,
+                                                                             segment.delayMs, 0};
+                }
+                input_compose.effects[input_compose.num_of_effects++] = {FF_PERIODIC, (int)id,
+                                                                         scale, duration, 0};
+            } else {  // Cirrus OWT
+                effect_str.append(segment.delayMs == 0
+                                          ? std::format("{}.{},", cirrusEffectId(id), scale)
+                                          : std::format("{} {}.{},", segment.delayMs,
+                                                        cirrusEffectId(id), scale));
+            }
+        }
+
+        setAmplitude(1);
+        if (mUsesCommonFFInterface) {
+            effectData.assign((int16_t*)&input_compose,
+                              (int16_t*)&input_compose + sizeof(input_compose) / sizeof(int16_t));
+        } else {
+            len = get_owt_data(effect_str.data(), (uint8_t*)data);
+            if (!len) return ndk::ScopedAStatus::fromExceptionCode(EX_SERVICE_SPECIFIC);
+            effectData.assign(data, data + len);
+        }
+        uploadFFEffect(effectData, 0);
+        activate(1);
+        if (callback != nullptr) {
+            std::thread([=] {
+                usleep(totalDuration * 1000);
+                callback->onComplete();
+            }).detach();
+        }
+        return ndk::ScopedAStatus::ok();
     }
 
-    *status = ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-    return 0;
-}
+    ndk::ScopedAStatus Vibrator::getSupportedAlwaysOnEffects(
+            std::vector<Effect>* /*_aidl_return*/) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::alwaysOnEnable(int32_t /*id*/, Effect /*effect*/,
+                                                EffectStrength /*strength*/) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::alwaysOnDisable(int32_t /*id*/) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::getResonantFrequency(float* /*_aidl_return*/) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::getQFactor(float* /*_aidl_return*/) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::getFrequencyResolution(float* /*_aidl_return*/) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::getFrequencyMinimum(float* /*_aidl_return*/) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::getBandwidthAmplitudeMap(std::vector<float>* /*_aidl_return*/) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::getPwlePrimitiveDurationMax(int32_t* /*_aidl_return*/) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::getPwleCompositionSizeMax(int32_t* /*_aidl_return*/) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::getSupportedBraking(std::vector<Braking>* /*_aidl_return*/) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::composePwle(
+            const std::vector<PrimitivePwle>& /*composite*/,
+            const std::shared_ptr<IVibratorCallback>& /*callback*/) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::activate(uint32_t timeoutMs) {
+        std::lock_guard<std::mutex> lock{mMutex};
+        if (mIsTimedOutVibrator) {
+            return writeNode(VIBRATOR_TIMEOUT_PATH, timeoutMs);
+        }
+        if (mIsForceFeedbackVibrator) {
+            struct input_event event{
+                    .type = EV_FF,
+                    .code = 0,
+                    .value = timeoutMs != 0,
+            };
+            if (write(mVibratorFd, &event, sizeof(event)) == -1)
+                return ndk::ScopedAStatus::fromExceptionCode(STATUS_UNKNOWN_ERROR);
+            return ndk::ScopedAStatus::ok();
+        }
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    ndk::ScopedAStatus Vibrator::uploadFFEffect(std::vector<int16_t> effectData, int timeoutMs) {
+        int ret;
+
+        // Remove previously uploaded effect in case it exists
+        ret = ioctl(mVibratorFd, EVIOCRMFF, 0);
+        if (ret == -1) {
+            LOG(WARNING) << "Failed to remove effect";
+        }
+
+        struct ff_effect effect = {
+                .id = -1,
+                .replay.length = static_cast<uint16_t>(timeoutMs),
+        };
+
+        if (mUsesCommonFFInterface && effectData[0] == 0 && effectData.size() <= sizeof(int)) {
+            effect.type = FF_CONSTANT;
+        } else {
+            effect.type = FF_PERIODIC;
+            effect.u.periodic = {
+                    .waveform = FF_CUSTOM,
+                    .custom_len = static_cast<uint32_t>(effectData.size()),
+                    .custom_data = effectData.data(),
+            };
+        }
+
+        ret = ioctl(mVibratorFd, EVIOCSFF, &effect);
+        if (ret == -1) {
+            LOG(ERROR) << "Effect upload failed: " << errno;
+            return ndk::ScopedAStatus::fromStatus(STATUS_UNKNOWN_ERROR);
+        }
+        return ndk::ScopedAStatus::ok();
+    }
+
+    float Vibrator::strengthToAmplitude(EffectStrength strength, ndk::ScopedAStatus * status) {
+        *status = ndk::ScopedAStatus::ok();
+
+        switch (strength) {
+            case EffectStrength::LIGHT:
+                return AMPLITUDE_LIGHT;
+            case EffectStrength::MEDIUM:
+                return AMPLITUDE_MEDIUM;
+            case EffectStrength::STRONG:
+                return AMPLITUDE_STRONG;
+        }
+
+        *status = ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+        return 0;
+    }
+
+    uint32_t Vibrator::effectToMs(Effect effect, ndk::ScopedAStatus * status) {
+        *status = ndk::ScopedAStatus::ok();
+        switch (effect) {
+            case Effect::CLICK:
+                return getIntProperty("click" + kVibratorPropDuration, 10);
+            case Effect::TICK:
+                return getIntProperty("tick" + kVibratorPropDuration, 5);
+            case Effect::TEXTURE_TICK:
+                return getIntProperty("texture_tick" + kVibratorPropDuration, 5);
+            default:
+                break;
+        }
+        *status = ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+        return 0;
+    }
+
+    uint32_t Vibrator::cirrusEffectId(uint32_t id) {
+        /* drivers/vibrator/cs/cs40l26/cs40l26.c - cs40l26_index_mapping */
+        switch (id) {
+            case 0:  // NOOP effect
+            case 1:  // CLICK effect
+            case 100:
+                break;
+            case 119 ... 124:
+                id += 16;
+                break;
+            case 126 ... 127:
+                id += 15;
+                break;
+            default:
+                id += 9;
+                break;
+        }
+        return id;
+    }
 
 #ifdef VIBRATOR_SUPPORTS_DURATION_AMPLITUDE_CONTROL
-float Vibrator::durationAmplitude(float amplitude) {
-    if (amplitude == 1) {
-        return DURATION_AMPLITUDE_STRONG;
-    } else if (amplitude >= 0.5) {
-        return DURATION_AMPLITUDE_MEDIUM;
-    }
+    float Vibrator::durationAmplitude(float amplitude) {
+        if (amplitude == 1) {
+            return DURATION_AMPLITUDE_STRONG;
+        } else if (amplitude >= 0.5) {
+            return DURATION_AMPLITUDE_MEDIUM;
+        }
 
-    return DURATION_AMPLITUDE_LIGHT;
-}
+        return DURATION_AMPLITUDE_LIGHT;
+    }
 #endif
 
 }  // namespace vibrator
