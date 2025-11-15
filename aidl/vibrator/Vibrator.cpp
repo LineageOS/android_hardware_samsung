6d5b0e10e7be18402c54c92c09ac81c8944ec764
borbelyvince <borbely.vince.11@gmail.com>
2025-11-15 11:46:32 +0100

    aidl: vibrator: Add support for composite effects
    OWT imported from https://github.com/CirrusLogic/cirrus-hal/tree/owt_lib,
    the first parameter of dspmem_chunk_create changed to uint8_t * to avoid
    void pointer arithmetic.
    
    Co-authored-by: ExtremeXT <extremext@lineageos.org>
    Change-Id: I19614b2f593991ed1912b22e701f5e653b45227b
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- aidl/vibrator/Vibrator.cpp
+++ aidl/vibrator/Vibrator.cpp
@@ -5,6 +5,9 @@
  */
 
 #include "Vibrator.h"
+extern "C" {
+#include "owt.h"
+}
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
@@ -40,6 +43,14 @@ static std::map<Effect, std::pair<short, int>> FF_EFFECT_IDS{{Effect::CLICK, {1,
                                                              {Effect::TICK, {41, 25}},
                                                              {Effect::HEAVY_CLICK, {14, 100}},
                                                              {Effect::TEXTURE_TICK, {41, 25}}};
+
+std::map<CompositePrimitive, std::pair<short, int>> FF_PRIMITIVE_IDS{
+        {CompositePrimitive::NOOP, {0, 0}},           {CompositePrimitive::CLICK, {1, 20}},
+        {CompositePrimitive::THUD, {124, 300}},       {CompositePrimitive::SPIN, {123, 130}},
+        {CompositePrimitive::QUICK_RISE, {121, 150}}, {CompositePrimitive::SLOW_RISE, {122, 500}},
+        {CompositePrimitive::QUICK_FALL, {120, 100}}, {CompositePrimitive::LIGHT_TICK, {41, 20}},
+        {CompositePrimitive::LOW_TICK, {119, 20}},
+};
 
 #ifdef VIBRATOR_SUPPORTS_DURATION_AMPLITUDE_CONTROL
 static std::map<EffectStrength, float> DURATION_AMPLITUDE = {
@@ -98,6 +109,10 @@ Vibrator::Vibrator() {
                         } else {
                             writeNode("/sys/class/sec_vib_inputff/control/use_sep_index", 1);
                         }
+                        if (ReadFileToString(VIBRATOR_FUNCTIONS_PATH, &contents) &&
+                            contents.find("PRIMITIVE_EFFECT_COMPOSE") != std::string::npos) {
+                            mSupportsPrimitives = true;
+                        }
                     }
                     break;
                 }
@@ -125,6 +140,9 @@ ndk::ScopedAStatus Vibrator::getCapabilities(int32_t* _aidl_return) {
 
     if (mIsForceFeedbackVibrator) {
         *_aidl_return |= IVibrator::CAP_AMPLITUDE_CONTROL;
+        if (mSupportsPrimitives) {
+            *_aidl_return |= IVibrator::CAP_COMPOSE_EFFECTS;
+        }
     }
 
     return ndk::ScopedAStatus::ok();
@@ -292,27 +310,116 @@ ndk::ScopedAStatus Vibrator::setExternalControl(bool enabled) {
     return ndk::ScopedAStatus::ok();
 }
 
-ndk::ScopedAStatus Vibrator::getCompositionDelayMax(int32_t* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+ndk::ScopedAStatus Vibrator::getCompositionDelayMax(int32_t* _aidl_return) {
+    if (!mSupportsPrimitives)
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+
+    *_aidl_return = 1000;
+    return ndk::ScopedAStatus::ok();
 }
 
-ndk::ScopedAStatus Vibrator::getCompositionSizeMax(int32_t* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+ndk::ScopedAStatus Vibrator::getCompositionSizeMax(int32_t* _aidl_return) {
+    if (!mSupportsPrimitives)
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+
+    *_aidl_return = 10;
+    return ndk::ScopedAStatus::ok();
 }
 
-ndk::ScopedAStatus Vibrator::getSupportedPrimitives(
-        std::vector<CompositePrimitive>* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+ndk::ScopedAStatus Vibrator::getSupportedPrimitives(std::vector<CompositePrimitive>* _aidl_return) {
+    if (!mSupportsPrimitives)
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+
+    for (auto primitive : FF_PRIMITIVE_IDS) _aidl_return->push_back(primitive.first);
+
+    return ndk::ScopedAStatus::ok();
 }
 
-ndk::ScopedAStatus Vibrator::getPrimitiveDuration(CompositePrimitive /*primitive*/,
-                                                  int32_t* /*_aidl_return*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+ndk::ScopedAStatus Vibrator::getPrimitiveDuration(CompositePrimitive primitive,
+                                                  int32_t* _aidl_return) {
+    if (!mSupportsPrimitives)
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+
+    *_aidl_return = FF_PRIMITIVE_IDS.at(primitive).second;
+    return ndk::ScopedAStatus::ok();
 }
 
-ndk::ScopedAStatus Vibrator::compose(const std::vector<CompositeEffect>& /*composite*/,
-                                     const std::shared_ptr<IVibratorCallback>& /*callback*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+ndk::ScopedAStatus Vibrator::compose(const std::vector<CompositeEffect>& composite,
+                                     const std::shared_ptr<IVibratorCallback>& callback) {
+    if (!mSupportsPrimitives)
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    if (composite.empty() || composite.size() > 10)
+        return ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_ARGUMENT);
+
+    struct common_inputff_effects input_compose = {};
+    int16_t data[WT_TYPE12_PWLE_SINGLE_PACKED_MAX / 2];
+    std::string effect_str;
+    int len, ms = 0;
+
+    for (auto segment : composite) {
+        if (segment.delayMs < 0 || segment.delayMs > 1000)
+            return ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_ARGUMENT);
+        if (FF_PRIMITIVE_IDS.find(segment.primitive) == FF_PRIMITIVE_IDS.end() ||
+            segment.scale < 0 || segment.scale > 1)
+            return ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_ARGUMENT);
+
+        if (segment.primitive == CompositePrimitive::NOOP) {
+            // No haptic effect. Used to generate extended delays between primitives.
+            if (segment.delayMs > 0) {
+                if (mUsesCommonFFInterface)
+                    input_compose.effects[input_compose.num_of_effects++] = {0, 0, 0,
+                                                                             segment.delayMs, 0};
+                else
+                    effect_str.append(std::format("{} ", segment.delayMs));
+            }
+            ms += segment.delayMs;
+            continue;
+        }
+
+        // Consider 15% the lowest "feelable" amplitude
+        int scale = round(segment.scale * 85 + 15);
+
+        uint32_t id = FF_PRIMITIVE_IDS.at(segment.primitive).first;
+
+        int duration = 0;
+        getPrimitiveDuration(segment.primitive, &duration);
+        ms += duration + segment.delayMs;
+
+        if (mUsesCommonFFInterface) {
+            if (segment.delayMs > 0) {
+                input_compose.effects[input_compose.num_of_effects++] = {0, 0, 0, segment.delayMs,
+                                                                         0};
+            }
+            input_compose.effects[input_compose.num_of_effects++] = {FF_PERIODIC, (int)id, scale,
+                                                                     duration, 0};
+        } else {  // Cirrus OWT
+            effect_str.append(
+                    segment.delayMs == 0
+                            ? std::format("{}.{} ", cirrusEffectId(id), scale)
+                            : std::format("{} {}.{} ", segment.delayMs, cirrusEffectId(id), scale));
+        }
+    }
+
+    if (mUsesCommonFFInterface) {
+        setAmplitude(1);
+        std::vector<int16_t> commonEffectData(
+                (int16_t*)&input_compose,
+                (int16_t*)&input_compose + sizeof(input_compose) / sizeof(int16_t));
+        uploadFFEffect(commonEffectData, 0);
+    } else {
+        len = get_owt_data(effect_str.data(), (uint8_t*)data);
+        if (!len) return ndk::ScopedAStatus::fromExceptionCode(EX_SERVICE_SPECIFIC);
+        std::vector<int16_t> effectData(data, data + len);
+        uploadFFEffect(effectData, 0);
+    }
+    activate(1);
+    if (callback != nullptr) {
+        std::thread([=] {
+            usleep(ms * 1000);
+            callback->onComplete();
+        }).detach();
+    }
+    return ndk::ScopedAStatus::ok();
 }
 
 ndk::ScopedAStatus Vibrator::getSupportedAlwaysOnEffects(std::vector<Effect>* /*_aidl_return*/) {
@@ -397,7 +504,8 @@ ndk::ScopedAStatus Vibrator::uploadFFEffect(std::vector<int16_t> effectData, int
             .replay.length = static_cast<uint16_t>(timeoutMs),
     };
 
-    if (mUsesCommonFFInterface && effectData[0] == 0) {
+    if (mUsesCommonFFInterface && effectData[0] == 0 &&
+        effectData.size() != sizeof(common_inputff_effects) / 2) {
         effect.type = FF_CONSTANT;
     } else {
         effect.type = FF_PERIODIC;
@@ -446,6 +554,15 @@ uint32_t Vibrator::effectToMs(Effect effect, ndk::ScopedAStatus* status) {
     }
     *status = ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
     return 0;
+}
+
+uint32_t Vibrator::cirrusEffectId(uint32_t id) {
+    if (id >= FF_PRIMITIVE_IDS.at(CompositePrimitive::LOW_TICK).first &&
+        id <= FF_PRIMITIVE_IDS.at(CompositePrimitive::THUD).first)
+        id += 16;
+    else if (id == FF_PRIMITIVE_IDS.at(CompositePrimitive::LIGHT_TICK).first)
+        id += 9;
+    return id;
 }
 
 #ifdef VIBRATOR_SUPPORTS_DURATION_AMPLITUDE_CONTROL
