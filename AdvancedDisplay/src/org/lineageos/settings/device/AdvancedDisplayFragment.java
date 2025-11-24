5119efc1e733968058b76b77e15de0deab7e9a2d
ExtremeXT <extremextdev@gmail.com>
2025-11-24 15:35:18 +0200

    AdvancedDisplay: Convert AdvancedDisplay for M3E support
    Change-Id: Iae0ef91be9f9b32ffdd0095be7518705f75a73ee
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- AdvancedDisplay/src/org/lineageos/settings/device/AdvancedDisplayFragment.java
+++ AdvancedDisplay/src/org/lineageos/settings/device/AdvancedDisplayFragment.java
@@ -19,13 +19,13 @@ package org.lineageos.settings.device;
 import android.content.res.Resources;
 import android.os.Bundle;
 
-import androidx.preference.PreferenceFragment;
+import com.android.settingslib.widget.SettingsBasePreferenceFragment;
 
 import org.lineageos.settings.device.R;
 
 import org.lineageos.internal.util.FileUtils;
 
-public class AdvancedDisplayFragment extends PreferenceFragment {
+public class AdvancedDisplayFragment extends SettingsBasePreferenceFragment {
     private mDNIeScenario mmDNIeScenario;
     private mDNIeAccessibility mmDNIeAccessibility;
 
