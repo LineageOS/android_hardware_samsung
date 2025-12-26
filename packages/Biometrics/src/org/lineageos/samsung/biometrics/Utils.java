b38dfe746b83dc4f93eb971c3d5816afe8cf4b2a
Atakan <ata.kaner03@gmail.com>
2025-12-26 12:41:29 +0300

    SamsungBiometrics: Implement basic utils
    Change-Id: Ic6ac6c6d74fad0ab88fab05d62f82e83e7e829e8

---
+++ packages/Biometrics/src/org/lineageos/samsung/biometrics/Utils.java
@@ -0,0 +1,48 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.samsung.biometrics;
+
+import android.util.Slog;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Utils {
+    private static final String TAG = "SB_Utils";
+
+    public static byte[] readFile(File file) throws IOException {
+        byte[] bArr = null;
+        if (!file.exists()) {
+            Slog.i(TAG, "Invalid file info, " + file);
+            return null;
+        }
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            bArr = new byte[(int) file.length()];
+            fileInputStream.read(bArr);
+            fileInputStream.close();
+            return bArr;
+        } catch (IOException e) {
+            Slog.w(TAG, "failed to read file", e);
+            return bArr;
+        }
+    }
+
+    public static void writeFile(File file, byte[] bArr) throws IOException {
+        if (bArr == null) {
+            return;
+        }
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(bArr);
+            fileOutputStream.close();
+        } catch (IOException e) {
+            Slog.w(TAG, "failed to write file", e);
+        }
+    }
+}
