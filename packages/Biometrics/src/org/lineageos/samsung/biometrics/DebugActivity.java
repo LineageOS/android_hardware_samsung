492b02757650b228e56ca6e9f4b686b6999aafea
Atakan <ata.kaner03@gmail.com>
2025-12-26 12:43:14 +0300

    SamsungBiometrics: Implement debugging for MaskView
    Change-Id: I03bf204463c7071b1a9fd8da037b0de945a17d75

--- packages/Biometrics/src/org/lineageos/samsung/biometrics/DebugActivity.java
+++ packages/Biometrics/src/org/lineageos/samsung/biometrics/DebugActivity.java
@@ -6,15 +6,50 @@
 package org.lineageos.samsung.biometrics;
 
 import android.app.Activity;
+import android.graphics.Insets;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.Switch;
 
 public class DebugActivity extends Activity {
-    private static final String TAG = "DebugActivity";
+    private MaskView mMaskView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // We will create a layout later
-        // setContentView(R.layout.activity_debug);
+        setContentView(R.layout.activity_debug);
+
+        mMaskView = new MaskView(this);
+        final Switch maskViewToggle = findViewById(R.id.mask_view_toggle);
+        maskViewToggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                mMaskView.show();
+            } else {
+                mMaskView.hide();
+             }
+        });
+
+        final View root = findViewById(R.id.main);
+        if (root != null) {
+            root.setOnApplyWindowInsetsListener((v, insets) -> {
+                Insets sys = insets.getInsets(WindowInsets.Type.systemBars());
+                v.setPadding(sys.left, sys.top, sys.right, sys.bottom);
+                return insets;
+            });
+            root.requestApplyInsets();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mMaskView != null) mMaskView.hide();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mMaskView != null) mMaskView.stop();
     }
 }
