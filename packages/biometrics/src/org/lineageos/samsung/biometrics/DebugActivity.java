aa6a281ccbcee2fce50ddc8223ef8e1e5597dd28
Atakan <ata.kaner03@gmail.com>
2025-12-26 09:18:57 +0300

    [WIP] SamsungBiometrics: Add debug activity skeleton
    Change-Id: I43da9ade5a9dfe3d0a30454e6679dbde4af5c6b3

---
+++ packages/biometrics/src/org/lineageos/samsung/biometrics/DebugActivity.java
@@ -0,0 +1,15 @@
+package org.lineageos.samsung.biometrics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DebugActivity extends Activity {
+    private static final String TAG = "DebugActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // We will create a layout later
+        // setContentView(R.layout.activity_debug);
+    }
+}
