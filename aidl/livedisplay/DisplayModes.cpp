b19c76ffe6d6ba05c1f438472ef7336cca103801
CmdCtrlDevic3 <cmdctrldevic3@proton.me>
2025-12-31 00:13:36 +0200

    livedisplay: drop Reading display profile
    No device from 8895 onwards has reading profile in panel drivers.
    
    Change-Id: I1cfd76384b619a522d21d616abc390cd321cd9ed
    Signed-off-by: CmdCtrlDevic3 <cmdctrldevic3@proton.me>

--- aidl/livedisplay/DisplayModes.cpp
+++ aidl/livedisplay/DisplayModes.cpp
@@ -27,7 +27,6 @@ const std::map<int32_t, std::string> DisplayModes::kModeMap = {
     {2, "Natural"},
     {3, "Cinema"},
     {4, "Adaptive"},
-    {5, "Reading"},
         // clang-format on
 };
 
