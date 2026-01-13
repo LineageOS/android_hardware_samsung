e8a45563ec85b2754caa88d8d467a99ed3f689f3
Bruno Martins <bgcngm@gmail.com>
2026-01-13 22:09:02 +0000

    hidl: Run through clang-format
    Change-Id: Ia848230ceb454b858d535481b34f193e06e765c6

--- hidl/sensors/1.0/Sensors.h
+++ hidl/sensors/1.0/Sensors.h
@@ -29,7 +29,6 @@ namespace sensors {
 namespace V1_0 {
 namespace implementation {
 
-
 struct Sensors : public ::android::hardware::sensors::V1_0::ISensors {
     Sensors();
 
@@ -39,45 +38,41 @@ struct Sensors : public ::android::hardware::sensors::V1_0::ISensors {
 
     Return<Result> setOperationMode(OperationMode mode) override;
 
-    Return<Result> activate(
-            int32_t sensor_handle, bool enabled) override;
+    Return<Result> activate(int32_t sensor_handle, bool enabled) override;
 
     Return<void> poll(int32_t maxCount, poll_cb _hidl_cb) override;
 
-    Return<Result> batch(
-            int32_t sensor_handle,
-            int64_t sampling_period_ns,
-            int64_t max_report_latency_ns) override;
+    Return<Result> batch(int32_t sensor_handle, int64_t sampling_period_ns,
+                         int64_t max_report_latency_ns) override;
 
     Return<Result> flush(int32_t sensor_handle) override;
 
     Return<Result> injectSensorData(const Event& event) override;
 
-    Return<void> registerDirectChannel(
-            const SharedMemInfo& mem, registerDirectChannel_cb _hidl_cb) override;
+    Return<void> registerDirectChannel(const SharedMemInfo& mem,
+                                       registerDirectChannel_cb _hidl_cb) override;
 
     Return<Result> unregisterDirectChannel(int32_t channelHandle) override;
 
-    Return<void> configDirectReport(
-            int32_t sensorHandle, int32_t channelHandle, RateLevel rate,
-            configDirectReport_cb _hidl_cb) override;
+    Return<void> configDirectReport(int32_t sensorHandle, int32_t channelHandle, RateLevel rate,
+                                    configDirectReport_cb _hidl_cb) override;
 
-private:
+  private:
     static constexpr int32_t kPollMaxBufferSize = 128;
     status_t mInitCheck;
-    sensors_module_t *mSensorModule;
-    sensors_poll_device_1_t *mSensorDevice;
+    sensors_module_t* mSensorModule;
+    sensors_poll_device_1_t* mSensorDevice;
     std::mutex mPollLock;
 
     int getHalDeviceVersion() const;
 
-    static void convertFromSensorEvents(
-            size_t count, const sensors_event_t *src, hidl_vec<Event> *dst);
+    static void convertFromSensorEvents(size_t count, const sensors_event_t* src,
+                                        hidl_vec<Event>* dst);
 
     DISALLOW_COPY_AND_ASSIGN(Sensors);
 };
 
-extern "C" ISensors *HIDL_FETCH_ISensors(const char *name);
+extern "C" ISensors* HIDL_FETCH_ISensors(const char* name);
 
 }  // namespace implementation
 }  // namespace V1_0
