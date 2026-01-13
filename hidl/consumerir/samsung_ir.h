4b128b05b3d1160044e24ad4bd2aa65da835fb2f
Bruno Martins <bgcngm@gmail.com>
2026-01-13 22:09:02 +0000

    hidl: Run through clang-format
    Change-Id: Ia848230ceb454b858d535481b34f193e06e765c6

--- hidl/consumerir/samsung_ir.h
+++ hidl/consumerir/samsung_ir.h
@@ -37,13 +37,9 @@ using android::hardware::ir::V1_0::ConsumerIrFreqRange;
  * can override this header in your device tree
  */
 // Some devices need MS_IR_SIGNAL to avoid ms to pulses conversionn
-//#define MS_IR_SIGNAL
+// #define MS_IR_SIGNAL
 
 static const std::vector<ConsumerIrFreqRange> consumerirFreqs = {
-    {.min = 30000, .max = 30000},
-    {.min = 33000, .max = 33000},
-    {.min = 36000, .max = 36000},
-    {.min = 38000, .max = 38000},
-    {.min = 40000, .max = 40000},
-    {.min = 56000, .max = 56000},
+        {.min = 30000, .max = 30000}, {.min = 33000, .max = 33000}, {.min = 36000, .max = 36000},
+        {.min = 38000, .max = 38000}, {.min = 40000, .max = 40000}, {.min = 56000, .max = 56000},
 };
