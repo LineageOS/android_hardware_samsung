d1191e676da1b389dac410715dfd52ff9d261a1c
ExtremeXT <extremextdev@gmail.com>
2026-01-19 22:17:59 +0200

    aidl: Initial Lineage health implementation
    Change-Id: I80630a1becf2b6ae6e01f7d60741a036dc232379
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

---
+++ aidl/lineage_health/ChargingControl.cpp
@@ -0,0 +1,75 @@
+/*
+ * SPDX-FileCopyrightText: 2022-2026 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "ChargingControl.h"
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+
+#define LOG_TAG "vendor.lineage.health-service.samsung"
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace health {
+
+ndk::ScopedAStatus ChargingControl::getChargingEnabled(bool* _aidl_return) {
+    std::string content;
+    if (!android::base::ReadFileToString(CHARGING_ENABLED_NODE, &content, true)) {
+        LOG(ERROR) << "Failed to read current charging enabled value";
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    if (content == "1") {
+        *_aidl_return = true;
+    } else if (content == "0") {
+        *_aidl_return = false;
+    } else {
+        LOG(ERROR) << "Unknown value " << content;
+        return ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_STATE);
+    }
+
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus ChargingControl::setChargingEnabled(bool enabled) {
+    if (!android::base::WriteStringToFile(std::to_string(enabled), CHARGING_ENABLED_NODE, true)) {
+        LOG(ERROR) << "Failed to write to charging enable node: " << strerror(errno);
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus ChargingControl::getChargingDeadline(int64_t* /* deadline */) {
+    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+}
+
+ndk::ScopedAStatus ChargingControl::setChargingDeadline(int64_t /* deadline */) {
+    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+}
+
+ndk::ScopedAStatus ChargingControl::getChargingLimit(ChargingLimitInfo* /* _aidl_return */) {
+    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+}
+
+ndk::ScopedAStatus ChargingControl::setChargingLimit(const ChargingLimitInfo& /* limit */) {
+    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+}
+
+ndk::ScopedAStatus ChargingControl::getSupportedMode(int* _aidl_return) {
+    if (access(CHARGING_ENABLED_NODE, O_RDWR)) {
+        LOG(ERROR) << "Can't access " << CHARGING_ENABLED_NODE << ": " << strerror(errno);
+    } else {
+        *_aidl_return = static_cast<int>(ChargingControlSupportedMode::TOGGLE) |
+                        static_cast<int>(ChargingControlSupportedMode::BYPASS);
+    }
+    return ndk::ScopedAStatus::ok();
+}
+
+}  // namespace health
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
---
+++ aidl/lineage_health/FastCharge.h
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: 2026 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/lineage/health/BnFastCharge.h>
+
+#define AFC_DISABLE_NODE "/sys/class/sec/switch/afc_disable"
+#define PD_DISABLE_NODE "/sys/class/power_supply/battery/pd_disable"
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace health {
+
+class FastCharge : public BnFastCharge {
+  public:
+    FastCharge();
+
+    ndk::ScopedAStatus getSupportedFastChargeModes(int64_t* _aidl_return) override;
+    ndk::ScopedAStatus getFastChargeMode(FastChargeMode* _aidl_return) override;
+    ndk::ScopedAStatus setFastChargeMode(FastChargeMode in_mode,
+                                         FastChargeMode* _aidl_return) override;
+
+  private:
+    int32_t mSupportedModes;
+};
+
+}  // namespace health
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
