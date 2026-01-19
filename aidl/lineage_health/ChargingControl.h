51a980a03a28bdf4c2aeec07212a690b401f0888
ExtremeXT <extremextdev@gmail.com>
2026-01-19 22:17:59 +0200

    aidl: Initial Lineage health implementation
    Change-Id: I80630a1becf2b6ae6e01f7d60741a036dc232379
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

---
+++ aidl/lineage_health/ChargingControl.h
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2022-2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/lineage/health/BnChargingControl.h>
+#include <aidl/vendor/lineage/health/ChargingControlSupportedMode.h>
+#include <aidl/vendor/lineage/health/ChargingLimitInfo.h>
+
+#define CHARGING_ENABLED_NODE "/sys/class/power_supply/battery/charging_enabled"
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace health {
+
+class ChargingControl : public BnChargingControl {
+  public:
+    ndk::ScopedAStatus getChargingEnabled(bool* _aidl_return) override;
+    ndk::ScopedAStatus setChargingEnabled(bool enabled) override;
+    ndk::ScopedAStatus setChargingDeadline(int64_t deadline) override;
+    ndk::ScopedAStatus getSupportedMode(int* _aidl_return) override;
+    ndk::ScopedAStatus getChargingDeadline(int64_t* _aidl_return) override;
+    ndk::ScopedAStatus getChargingLimit(ChargingLimitInfo* _aidl_return) override;
+    ndk::ScopedAStatus setChargingLimit(const ChargingLimitInfo& limit) override;
+};
+
+}  // namespace health
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
