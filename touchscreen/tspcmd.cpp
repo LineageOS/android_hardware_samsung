817c5c4f43a76579659efdbbc185c3a77a92a7d1
borbelyvince <borbely.vince.11@gmail.com>
2026-02-02 15:31:59 +0100

    Add library for sending commands to the tsp
    Change-Id: Ic1c678c6f5d51ea17ce15017bcb9f8d01a2d3a80

---
+++ touchscreen/tspcmd.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2026 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "libsamsungtspcmd"
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <fstream>
+#include <utility>
+
+#include "tspcmd.h"
+
+using android::base::WriteStringToFd;
+
+TspCmdHandler::TspCmdHandler() {
+    std::ifstream stream("/sys/class/sec/tsp/cmd_list");
+
+    std::string temp;
+    while (std::getline(stream, temp)) {
+        if (temp.starts_with("++")) continue;
+        if (!temp.empty()) mSupportedCommands.emplace(temp);
+    }
+    mFd = unique_fd(open("/sys/class/sec/tsp/cmd", O_WRONLY));
+    if (!mFd.ok()) {
+        LOG(ERROR) << "Failed to open /sys/class/sec/tsp/cmd: " << strerror(errno);
+    }
+}
+
+bool TspCmdHandler::sendCommand(const std::string &command, const std::vector<std::string> &parameters) {
+    if (!isCommandSupported(command)) return false;
+
+    std::string cmd = command;
+    for (auto &param : parameters) {
+        cmd.append(",").append(param);
+    }
+
+    if (!WriteStringToFd(cmd, mFd)) {
+        LOG(ERROR) << "Failed to send command " << cmd << ": " << strerror(errno);
+        return false;
+    }
+    return true;
+}
