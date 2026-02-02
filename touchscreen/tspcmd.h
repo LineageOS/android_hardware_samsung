7a1f55e887e57b5804628bf4d8c64c01e4b33548
borbelyvince <borbely.vince.11@gmail.com>
2026-02-02 15:31:59 +0100

    Add library for sending commands to the tsp
    Change-Id: Ic1c678c6f5d51ea17ce15017bcb9f8d01a2d3a80

---
+++ touchscreen/tspcmd.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2026 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/logging.h>
+#include <android-base/unique_fd.h>
+#include <fcntl.h>
+#include <set>
+
+#pragma once
+
+using android::base::unique_fd;
+
+class TspCmdHandler {
+  public:
+    TspCmdHandler();
+
+    bool isCommandSupported(std::string command) { return mSupportedCommands.contains(command); }
+
+    bool sendCommand(const std::string& command, const std::vector<std::string>& parameters);
+    template <typename... T>
+    inline bool sendCommand(const std::string& command, T... parameters) {
+        std::vector<std::string> newParameters;
+        for (const auto& in : {parameters...}) {
+            newParameters.push_back(std::to_string(in));
+        }
+        return sendCommand(command, newParameters);
+    }
+
+  private:
+    unique_fd mFd;
+    std::set<std::string> mSupportedCommands;
+};
