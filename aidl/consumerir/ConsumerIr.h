6d3b75bbd38a00c5c8e613bba21a3ac3b5872597
j0sh1x <aljoshua.hell@gmail.com>
2026-05-18 17:15:06 +0200

    aidl: consumerir: Add AIDL implementation
    * Based on HIDL one removed in 491970
    
    Change-Id: Iaeaeeebd71da9b5d96ca500a81bcdbeda7cba23c
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

---
+++ aidl/consumerir/ConsumerIr.h
@@ -0,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/android/hardware/ir/BnConsumerIr.h>
+
+using ::aidl::android::hardware::ir::ConsumerIrFreqRange;
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace ir {
+
+class ConsumerIr : public BnConsumerIr {
+  public:
+    // Methods from aidl::android::hardware::ir::ConsumerIr follow.
+    ndk::ScopedAStatus getCarrierFreqs(
+            std::vector<::aidl::android::hardware::ir::ConsumerIrFreqRange>* _aidl_return)
+            override;
+    ndk::ScopedAStatus transmit(int32_t carrierFreqHz,
+                                const std::vector<int32_t>& pattern) override;
+  private:
+    bool isInRange(int32_t carrierFreqHz);
+};
+
+}  // namespace ir
+}  // namespace hardware
+}  // namespace android
+}  // namespace aidl
