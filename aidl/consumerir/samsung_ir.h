d4a5b5010d0b58d3b118ec527e167f4b8fad00bc
ExtremeXT <extremext@lineageos.org>
2026-05-18 17:15:06 +0200

    aidl: consumerir: Add AIDL implementation
    * Based on HIDL one removed in 491970
    
    Change-Id: Iaeaeeebd71da9b5d96ca500a81bcdbeda7cba23c
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

---
+++ aidl/consumerir/samsung_ir.h
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/android/hardware/ir/ConsumerIrFreqRange.h>
+#include <vector>
+
+using aidl::android::hardware::ir::ConsumerIrFreqRange;
+
+/*
+ * Board specific nodes
+ *
+ * If your device exposes these controls in another place, you can either
+ * symlink to the locations given here, or override this header in your
+ * device tree.
+ */
+#define IR_PATH "/sys/class/sec/sec_ir/ir_send"
+/*
+ * Board specific configs
+ *
+ * If your device needs a different configuration, you
+ * can override this header in your device tree
+ */
+// Some devices need MS_IR_SIGNAL to avoid ms to pulses conversionn
+// #define MS_IR_SIGNAL
+
+static const std::vector<ConsumerIrFreqRange> consumerirFreqs = {
+        {30000, 30000}, {33000, 33000}, {36000, 36000},
+        {38000, 38000}, {40000, 40000}, {56000, 56000},
+};
