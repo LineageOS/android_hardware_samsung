10a42d271730ad1eae78c71a18488bfca7f291b0
Secondarian <robertanitei@tutanota.com>
2026-08-15 14:14:45 +0200

    aidl: gatekeeper: Add TEEGRIS GK Skeleton
    * Derived from android.hardware.gatekeeper-service.nonsecure
    * Change-Id of the latest commit at the time of import: I81845d5e6370bdddb1a24d67437964e03a5fb243
    
    Change-Id: I71f75f6ffd81cf7b7eb8f3d1e37b25fe0573fa51
    Signed-off-by: Secondarian <robertanitei@tutanota.com>

---
+++ aidl/gatekeeper/teegris/service.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#define LOG_TAG "android.hardware.gatekeeper-service.teegris"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+
+#include "GateKeeper.h"
+
+using aidl::android::hardware::gatekeeper::TeegrisGateKeeperDevice;
+
+int main() {
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+
+    ::gatekeeper::TeegrisGateKeeper implementation;
+    auto gatekeeper = ndk::SharedRefBase::make<TeegrisGateKeeperDevice>(implementation);
+    const std::string instance = TeegrisGateKeeperDevice::descriptor + std::string("/default");
+    auto status = AServiceManager_addService(gatekeeper->asBinder().get(), instance.c_str());
+    CHECK_EQ(status, STATUS_OK);
+
+    ABinderProcess_joinThreadPool();
+    return -1;  // Should never get here.
+}
