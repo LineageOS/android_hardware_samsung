c2e07eae3d21a437f6c0d29019db48eece0480b3
ExtremeXT <extremext@lineageos.org>
2026-08-21 15:58:04 +0300

    aidl: gatekeeper: Adapt to Samsung
    Change-Id: I63d3a726e8d68a5515c4f6ff5bab1ce1b3b7198b
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

--- aidl/gatekeeper/service.cpp
+++ aidl/gatekeeper/service.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "android.hardware.gatekeeper-service.nonsecure"
+#define LOG_TAG "android.hardware.gatekeeper-service.samsung"
 
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
@@ -21,25 +21,16 @@
 #include <cutils/properties.h>
 
 #include "GateKeeper.h"
-#include "SharedSecret.h"
-#include "SoftGateKeeper.h"
 
-using aidl::android::hardware::gatekeeper::SoftGateKeeperDevice;
-using aidl::android::hardware::security::sharedsecret::SoftSharedSecret;
+using aidl::android::hardware::gatekeeper::SGatekeeper;
 
 int main(int, char** argv) {
     ::android::base::InitLogging(argv, ::android::base::KernelLogger);
     ABinderProcess_setThreadPoolMaxThreadCount(0);
 
-    auto secret = ndk::SharedRefBase::make<SoftSharedSecret>();
-    std::string secret_instance = SoftSharedSecret::descriptor + std::string("/gatekeeper");
-    auto status = AServiceManager_addService(secret->asBinder().get(), secret_instance.c_str());
-    CHECK_EQ(status, STATUS_OK);
-
-    ::gatekeeper::SoftGateKeeper implementation(*secret);
-    auto gatekeeper = ndk::SharedRefBase::make<SoftGateKeeperDevice>(implementation);
-    const std::string instance = SoftGateKeeperDevice::descriptor + std::string("/default");
-    status = AServiceManager_addService(gatekeeper->asBinder().get(), instance.c_str());
+    auto gatekeeper = ndk::SharedRefBase::make<SGatekeeper>();
+    const std::string instance = SGatekeeper::descriptor + std::string("/default");
+    auto status = AServiceManager_addService(gatekeeper->asBinder().get(), instance.c_str());
     CHECK_EQ(status, STATUS_OK);
 
     ABinderProcess_joinThreadPool();
