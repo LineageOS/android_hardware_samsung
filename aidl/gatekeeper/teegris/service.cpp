1e73bab2907edbc9dc4ef1a276d1e25478b6ab6c
Secondarian <robertanitei@tutanota.com>
2026-08-24 18:16:09 +0200

    aidl: gatekeeper: teegris: RE Functionality
    Change-Id: Ie22a624b4b91efaf10c1ee7010ce80c26d70df78
    Signed-off-by: Secondarian <robertanitei@tutanota.com>

--- aidl/gatekeeper/teegris/service.cpp
+++ aidl/gatekeeper/teegris/service.cpp
@@ -18,12 +18,36 @@
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
+#include <dlfcn.h>
 
 #include "GateKeeper.h"
+
+void (*TEEC_AllocateSharedMemory)(void**, struct shared_mem*);
+void (*TEEC_CloseSession)(void**);
+void (*TEEC_FinalizeContext)(void**);
+void (*TEEC_InitializeContext)(void*, void**);
+void (*TEEC_InvokeCommand)(void**, int, void*, int*);
+void (*TEEC_ReleaseSharedMemory)(struct shared_mem*);
+void (*TEECS_OpenSession)(void**, void**, const char[], void*, size_t, int, void*, void*, int*);
 
 using aidl::android::hardware::gatekeeper::TeegrisGateKeeperDevice;
 
 int main() {
+    void* libteecl_handle = dlopen("/vendor/lib64/libteecl.so", RTLD_NOW);
+
+    TEEC_AllocateSharedMemory = (void (*)(void**, struct shared_mem*))dlsym(
+            libteecl_handle, "TEEC_AllocateSharedMemory");
+    TEEC_CloseSession = (void (*)(void**))dlsym(libteecl_handle, "TEEC_CloseSession");
+    TEEC_FinalizeContext = (void (*)(void**))dlsym(libteecl_handle, "TEEC_FinalizeContext");
+    TEEC_InitializeContext =
+            (void (*)(void*, void**))dlsym(libteecl_handle, "TEEC_InitializeContext");
+    TEEC_InvokeCommand =
+            (void (*)(void**, int, void*, int*))dlsym(libteecl_handle, "TEEC_InvokeCommand");
+    TEEC_ReleaseSharedMemory =
+            (void (*)(struct shared_mem*))dlsym(libteecl_handle, "TEEC_ReleaseSharedMemory");
+    TEECS_OpenSession = (void (*)(void**, void**, const char[], void*, size_t, int, void*, void*,
+                                  int*))dlsym(libteecl_handle, "TEECS_OpenSession");
+
     ABinderProcess_setThreadPoolMaxThreadCount(0);
 
     ::gatekeeper::TeegrisGateKeeper implementation;
@@ -33,5 +57,6 @@ int main() {
     CHECK_EQ(status, STATUS_OK);
 
     ABinderProcess_joinThreadPool();
+    dlclose(libteecl_handle);
     return -1;  // Should never get here.
 }
