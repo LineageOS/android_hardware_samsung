e2b70fefaba7e758c5d5281c63811f300197f953
Secondarian <robertanitei@tutanota.com>
2026-08-24 18:16:09 +0200

    aidl: gatekeeper: teegris: RE Functionality
    Change-Id: Ie22a624b4b91efaf10c1ee7010ce80c26d70df78
    Signed-off-by: Secondarian <robertanitei@tutanota.com>

--- aidl/gatekeeper/teegris/service.cpp
+++ aidl/gatekeeper/teegris/service.cpp
@@ -19,12 +19,35 @@
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
+#include <dlfcn.h>
 
 #include "GateKeeper.h"
+
+void (*TEEC_AllocateSharedMemory)(void**, void*);
+void (*TEEC_CloseSession)(void**);
+void (*TEEC_FinalizeContext)(void**);
+void (*TEEC_InitializeContext)(void*, void**);
+void (*TEEC_InvokeCommand)(void**, int, void*, int*);
+void (*TEEC_ReleaseSharedMemory)(void*);
+void (*TEECS_OpenSession)(void**, void**, const char[], void*, size_t, int, void*, void*, int*);
 
 using aidl::android::hardware::gatekeeper::TeegrisGateKeeperDevice;
 
 int main() {
+    void* libteecl_handle = dlopen("/vendor/lib64/libteecl.so", RTLD_NOW);
+
+    TEEC_AllocateSharedMemory =
+            (typeof(TEEC_AllocateSharedMemory))dlsym(libteecl_handle, "TEEC_AllocateSharedMemory");
+    TEEC_CloseSession = (typeof(TEEC_CloseSession))dlsym(libteecl_handle, "TEEC_CloseSession");
+    TEEC_FinalizeContext =
+            (typeof(TEEC_FinalizeContext))dlsym(libteecl_handle, "TEEC_FinalizeContext");
+    TEEC_InitializeContext =
+            (typeof(TEEC_InitializeContext))dlsym(libteecl_handle, "TEEC_InitializeContext");
+    TEEC_InvokeCommand = (typeof(TEEC_InvokeCommand))dlsym(libteecl_handle, "TEEC_InvokeCommand");
+    TEEC_ReleaseSharedMemory =
+            (typeof(TEEC_ReleaseSharedMemory))dlsym(libteecl_handle, "TEEC_ReleaseSharedMemory");
+    TEECS_OpenSession = (typeof(TEECS_OpenSession))dlsym(libteecl_handle, "TEECS_OpenSession");
+
     ABinderProcess_setThreadPoolMaxThreadCount(0);
 
     ::gatekeeper::TeegrisGateKeeper implementation;
@@ -34,5 +57,6 @@ int main() {
     CHECK_EQ(status, STATUS_OK);
 
     ABinderProcess_joinThreadPool();
+    dlclose(libteecl_handle);
     return -1;  // Should never get here.
 }
