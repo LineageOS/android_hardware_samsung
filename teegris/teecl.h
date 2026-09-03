f6d24be583ea2b229f8e2357c0a89ef71b29e576
Secondarian <robertanitei@tutanota.com>
2026-09-03 16:04:59 +0200

    teegris: Add 'libteecl' dummies
    Change-Id: Iee1b16987ff1695f194eb5c3f5551bf99321708a
    Signed-off-by: Secondarian <robertanitei@tutanota.com>

---
+++ teegris/teecl.h
@@ -0,0 +1,8 @@
+void TEEC_AllocateSharedMemory(void**, void*);
+void TEEC_CloseSession(void**);
+void TEEC_FinalizeContext(void**);
+void TEEC_InitializeContext(void*, void**);
+void TEEC_InvokeCommand(void**, int, void*, int*);
+void TEEC_RegisterSharedMemory(void**, void*);
+void TEEC_ReleaseSharedMemory(void*);
+void TEECS_OpenSession(void**, void**, const char*, void*, size_t, int, void*, void*, int*);
