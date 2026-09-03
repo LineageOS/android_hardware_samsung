9d8fb7d54b65d758d42db623f806e2e5daf09add
Secondarian <robertanitei@tutanota.com>
2026-09-03 16:04:59 +0200

    teegris: Add 'libteecl' dummies
    Change-Id: Iee1b16987ff1695f194eb5c3f5551bf99321708a
    Signed-off-by: Secondarian <robertanitei@tutanota.com>

---
+++ teegris/teecl.h
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define TEEC_VALUE_INPUT 1
+#define TEEC_VALUE_OUTPUT 2
+
+void TEEC_AllocateSharedMemory(void**, void*);
+void TEEC_CloseSession(void**);
+void TEEC_FinalizeContext(void**);
+void TEEC_InitializeContext(void*, void**);
+void TEEC_InvokeCommand(void**, int, void*, int*);
+void TEEC_RegisterSharedMemory(void**, void*);
+void TEEC_ReleaseSharedMemory(void*);
+void TEECS_OpenSession(void**, void**, const char*, void*, size_t, int, void*, void*, int*);
